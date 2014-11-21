1a1dbcf93545d7bd0f5f3d61a014862944642bef
Martin Junghanns <martin.junghanns@gmx.net>
2014-11-21 16:41:51 +0100

    updated pom.xml

--- gradoop-core/src/main/java/org/gradoop/storage/GraphStore.java
+++ gradoop-core/src/main/java/org/gradoop/storage/GraphStore.java
@@ -2,20 +2,48 @@ package org.gradoop.storage;
 
 import org.gradoop.model.Graph;
 import org.gradoop.model.Vertex;
-import org.gradoop.storage.exceptions.UnsupportedTypeException;
 
 /**
- * Created by martin on 05.11.14.
+ * A graph store is responsible for writing and reading graphs including
+ * vertices and edges.
  */
 public interface GraphStore {
+  /**
+   * Writes the given graph into the graph store.
+   *
+   * @param graph graph to write
+   */
   void writeGraph(final Graph graph);
 
-  void writeVertex(final Vertex vertex)
-    throws UnsupportedTypeException;
+  /**
+   * Writes the given vertex into the graph store.
+   *
+   * @param vertex vertex to write
+   */
+  void writeVertex(final Vertex vertex);
 
+  /**
+   * Reads a graph entity from the graph store using the given graph id. If
+   * {@code graphID} does not exist, {@code null} is returned.
+   *
+   * @param graphID graph id
+   * @return graph entity or {@code null} if there is no entity with the given
+   * {@code graphId}
+   */
   Graph readGraph(final Long graphID);
 
+  /**
+   * Reads a vertex entity from the graph store using the given vertex id. If
+   * {@code vertexID} does not exist, {@code null} is returned.
+   *
+   * @param vertexID vertex id
+   * @return vertex entity or {@code null} if there is no entity with the given
+   * {@code vertexID}
+   */
   Vertex readVertex(final Long vertexID);
 
+  /**
+   * Closes the graph store.
+   */
   void close();
 }
