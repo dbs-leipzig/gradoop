0a716712e27ccdfb2f2a6787f784cfc915a7dd54
Martin Junghanns <martin.junghanns@gmx.net>
2014-11-27 16:35:28 +0100

    added documentation

--- gradoop-core/src/main/java/org/gradoop/storage/exceptions/UnsupportedTypeException.java
+++ gradoop-core/src/main/java/org/gradoop/storage/exceptions/UnsupportedTypeException.java
@@ -1,9 +1,15 @@
 package org.gradoop.storage.exceptions;
 
 /**
- * Created by martin on 11.11.14.
+ * Used during parsing of properties. Is thrown when a type is not supported by
+ * Gradoop.
  */
 public class UnsupportedTypeException extends RuntimeException {
+  /**
+   * Creates a new exception object using the given message.
+   *
+   * @param message
+   */
   public UnsupportedTypeException(String message) {
     super(message);
   }
