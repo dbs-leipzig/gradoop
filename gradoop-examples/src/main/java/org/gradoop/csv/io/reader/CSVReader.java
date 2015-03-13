a270239794b7b89fff43f04ff300171a425f9a5d
Kevin Gomez <k.gomez@freenet.de>
2015-03-13 16:19:15 +0100

    started tests

--- gradoop-examples/src/main/java/org/gradoop/csv/io/reader/CSVReader.java
+++ gradoop-examples/src/main/java/org/gradoop/csv/io/reader/CSVReader.java
@@ -2,6 +2,7 @@ package org.gradoop.csv.io.reader;
 
 import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.log4j.Logger;
 import org.gradoop.io.reader.ConfigurableVertexLineReader;
 import org.gradoop.model.Vertex;
 import org.gradoop.model.impl.VertexFactory;
@@ -16,72 +17,87 @@ import java.util.regex.Pattern;
  * Reads csv input data
  */
 public class CSVReader implements ConfigurableVertexLineReader {
-  private static final Pattern LINE_TOKEN_SEPARATOR = Pattern.compile("|");
+  private Logger Log = Logger.getLogger(CSVReader.class);
+  private static final Pattern LINE_TOKEN_SEPARATOR = Pattern.compile("\\|");
   private Configuration conf;
   private boolean once = true;
-  private boolean edge = false;
+  private boolean node = false;
   private List<String> labels;
   private String[] types;
   private String[] properties;
   private List<Vertex> vList;
 
+  public static final String META_DATA = "META_DATA";
+  public static final String TYPE = "TYPE";
+  public static final String LABEL = "LABEL";
+
   private String[] getTokens(String line) {
     return LINE_TOKEN_SEPARATOR.split(line);
   }
 
-  private void initialize(String firstLine) {
-    String metaData = conf.get("Meta");
-    this.labels.add(conf.get("Label"));
+  private void initializeAndReadFirstLine(String line) {
+    Log.info("###INITIALIZE");
+    vList = Lists.newArrayList();
+    labels = Lists.newArrayList();
+    Log.info("###ReadFirstLine");
+    this.properties = getTokens(line);
+    for (int i = 0; i < properties.length; i++) {
+      Log.info(properties[i]);
+    }
+    String metaData = conf.get(META_DATA);
+    labels.add(conf.get(LABEL));
+//    this.types =
+//      new String[]{"long", "string", "string", "string", "string", "string",
+//                   "string", "string",};
+
+
     try {
       BufferedReader in = new BufferedReader(new FileReader(metaData));
-      String line;
-      while ((line = in.readLine()) != null) {
-        this.types = getTokens(line);
+      String datLine;
+      while ((datLine = in.readLine()) != null) {
+        this.types = getTokens(datLine);
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
-    readFirstLine(firstLine);
-    checkProperties();
-  }
 
-  private void checkProperties() {
-    int ids = 0;
-    for (int j = 0; j < properties.length - 2; j++) {
-      if (properties[j].contains("id")) {
-        ids++;
-      }
-    }
-    if (ids > 1) {
-      edge = true;
-    }
-  }
-
-  private void readFirstLine(String line) {
-    vList = Lists.newArrayList();
+    checkType();
     once = false;
-    this.properties = getTokens(line);
+  }
+
+  private void checkType() {
+    Log.info("###checkType");
+    if (conf.get(TYPE).equals("node")) {
+      this.node = true;
+    }
+    Log.info(node);
   }
 
   private Vertex readRemainingLines(String line) {
+    Log.info("###ReadRemainingLines");
     String[] tokens = getTokens(line);
     long id = Long.parseLong(tokens[0]);
+    Log.info(id);
+    Log.info(labels.get(0));
     Vertex vex = VertexFactory.createDefaultVertexWithLabels(id, labels, null);
-    if (edge) {
-    } else {
-      for (int i = 1; i < properties.length - 2; i++) {
+    if (node) {
+      for (int i = 1; i < properties.length; i++) {
         switch (types[i]) {
         case "long":
           vex.addProperty(properties[i], Long.parseLong(tokens[i]));
+          Log.info(tokens[i]);
           break;
         case "string":
           vex.addProperty(properties[i], tokens[i]);
+          Log.info(tokens[i]);
           break;
         default:
           vex.addProperty(properties[i], tokens[i]);
           break;
         }
       }
+    } else {
+      //If edge
     }
     return vex;
   }
@@ -93,8 +109,9 @@ public class CSVReader implements ConfigurableVertexLineReader {
 
   @Override
   public List<Vertex> readVertexList(String line) {
-    initialize(line);
-    if (!once) {
+    if (once) {
+      initializeAndReadFirstLine(line);
+    } else {
       vList.add(readRemainingLines(line));
     }
     return vList;
--- gradoop-examples/src/test/java/csv/io/reader/CSVReaderTest.java
+++ gradoop-examples/src/test/java/csv/io/reader/CSVReaderTest.java
@@ -2,65 +2,65 @@ package csv.io.reader;
 
 import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.log4j.Logger;
 import org.gradoop.GradoopClusterTest;
 import org.gradoop.csv.io.reader.CSVReader;
 import org.gradoop.io.reader.ConfigurableVertexLineReader;
 import org.gradoop.model.Vertex;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
  * Tests for {@link org.gradoop.csv.io.reader.CSVReader}.
  */
 public class CSVReaderTest extends GradoopClusterTest {
+  Logger Log = Logger.getLogger(CSVReaderTest.class);
+  private final String NODE_META = getClass().getResource("/node_meta" +
+    ".csv").getPath();
 
-  private static final String NODE_META = "node_meta.csv";
-  private static final String NODE_TEXT = "person";
-
-
-
-
+  private static final String[] PERSON_CSV = new String[]{
+    "id|firstName|lastName|gender|birthday|creationDate|locationIP" +
+      "|browserUsed|",
+    "0|Arun|Reddy|female|1987-05-27|2010-03-22T12:42:14.255+0000|59.185.111" +
+      ".183|Firefox|",
+    "1|Yang|Li|male|1984-07-09|2010-03-07T13:58:27.400+0000|14.192.76" +
+      ".41|InternetExplorer|"};
 
   @Test
-  public void checkSimpleCSVInputTest() {
+  public void checkSimpleCSVInputTest() throws IOException, URISyntaxException {
+
+
+    Log.info(getClass().getResource("/node_meta.csv").getPath());
+
+
 
 
 
     for (Vertex v : createVerticesFromCSV()) {
-      if(v.getID() == 0){
-      }
+      System.out.println(v.getID());
     }
-
   }
 
-  private List<Vertex> createVerticesFromCSV(){
+  private List<Vertex> createVerticesFromCSV() {
     ConfigurableVertexLineReader reader = new CSVReader();
-
     Configuration conf = new Configuration();
-    conf.set("Meta", NODE_META);
-    conf.set("Label", "Person");
-
+    conf.set(CSVReader.META_DATA, NODE_META);
+    conf.set(CSVReader.LABEL, "Person");
+    conf.set(CSVReader.TYPE, "node");
     reader.setConf(conf);
-
     List<Vertex> vertices = Lists.newArrayList();
-
-    try {
-      BufferedReader in = new BufferedReader(new FileReader(NODE_TEXT));
-      String line;
-      while ((line = in.readLine()) != null) {
-        for (Vertex v : reader.readVertexList(line)) {
-          vertices.add(v);
-        }
+    for (String line : PERSON_CSV) {
+      for (Vertex v : reader.readVertexList(line)) {
+        vertices.add(v);
       }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
-
     return vertices;
   }
-
 }
