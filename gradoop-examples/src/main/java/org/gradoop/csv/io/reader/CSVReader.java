1d0ec13a4f6cd5db01007b656c36378913edd21c
Kevin Gomez <k.gomez@freenet.de>
2015-03-16 16:11:06 +0100

    CSVReader now supports node and edge csv files (#34) * deleted unnecessary files

--- gradoop-examples/src/main/java/org/gradoop/csv/io/reader/CSVReader.java
+++ gradoop-examples/src/main/java/org/gradoop/csv/io/reader/CSVReader.java
@@ -2,123 +2,263 @@ package org.gradoop.csv.io.reader;
 
 import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.log4j.Logger;
 import org.gradoop.io.reader.ConfigurableVertexLineReader;
+import org.gradoop.model.Edge;
 import org.gradoop.model.Vertex;
+import org.gradoop.model.impl.EdgeFactory;
 import org.gradoop.model.impl.VertexFactory;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Pattern;
 
 /**
  * Reads csv input data
  */
 public class CSVReader implements ConfigurableVertexLineReader {
-  private Logger Log = Logger.getLogger(CSVReader.class);
+  /**
+   * The path to the meta_data of a csv file
+   */
+  public static final String META_DATA = "csv-reader.meta_data";
+  /**
+   * The type of a csv input (nodes or edges)
+   */
+  public static final String TYPE = "csv-reader.type";
+  /**
+   * The label (relationship) of a csv input
+   */
+  public static final String LABEL = "csv-reader.label";
+  /**
+   * The expected amount of nodes that will be created
+   */
+  public static final String EXPECTED_SIZE = "csv-reader.expected_size";
+  /**
+   * Default value of the expected size
+   */
+  public static final int DEFAULT_EXPECTED_SIZE = 0;
+  /**
+   * Token Separator of a csv line
+   */
   private static final Pattern LINE_TOKEN_SEPARATOR = Pattern.compile("\\|");
+  /**
+   * Node type if csv input contains nodes
+   */
+  private static final String NODE_TYPE = "node";
+  /**
+   * Configuration
+   */
   private Configuration conf;
-  private boolean once = true;
-  private boolean node = false;
+  /**
+   * Boolean operator for initial step
+   */
+  private boolean initialStep = true;
+  /**
+   * Contains the information about the csv type (if node its true)
+   */
+  private boolean isNodeCSV = false;
+  /**
+   * String List for vertex creation
+   */
   private List<String> labels;
+  /**
+   * types of a csv file (e.g. long|string|string|integer)
+   */
   private String[] types;
+  /**
+   * properties of a csv input (headline)
+   */
   private String[] properties;
-  private List<Vertex> vList;
+  /**
+   * Random class declaration
+   */
+  private Random random;
 
-  public static final String META_DATA = "META_DATA";
-  public static final String TYPE = "TYPE";
-  public static final String LABEL = "LABEL";
-
+  /**
+   * Splits a line into tokens
+   *
+   * @param line line of csv input
+   * @return tokens as array
+   */
   private String[] getTokens(String line) {
     return LINE_TOKEN_SEPARATOR.split(line);
   }
 
-  private void initializeAndReadFirstLine(String line) {
-//    Log.info("###INITIALIZE");
-    vList = Lists.newArrayList();
+  /**
+   * Initial step: initializations and reading the headline
+   *
+   * @param line line of csv input
+   */
+  private void initialStep(String line) {
+    // Initialize Lists
     labels = Lists.newArrayList();
-//    Log.info("###ReadFirstLine");
+    // Get properties (e.g. FirstName, LastName...)
     properties = getTokens(line);
-//    for (int i = 0; i < properties.length; i++) {
-//      Log.info(properties[i]);
-//    }
-    String metaData = conf.get(META_DATA);
+    // Get MetaData (e.g. long, string, string...)
+    readTypes(conf.get(META_DATA));
+    // Set Labels
     labels.add(conf.get(LABEL));
+    // getCSVType
+    isNodeCSV = isNodeCSV();
+    random = new Random();
+    // initialStep is over
+    initialStep = false;
+  }
 
-
+  /**
+   * Load Metadata Types e.g. long|string|string|...
+   *
+   * @param metaData configuration parameter
+   */
+  private void readTypes(String metaData) {
     try {
-      BufferedReader in = new BufferedReader(new FileReader(metaData));
-      String datLine;
-      while ((datLine = in.readLine()) != null) {
-        types = getTokens(datLine);
+      BufferedReader in = new BufferedReader(
+        new InputStreamReader(new FileInputStream(metaData),
+          Charset.forName("UTF-8")));
+      String line;
+      while ((line = in.readLine()) != null) {
+        types = getTokens(line);
       }
+      in.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    checkType();
-    once = false;
   }
 
-  private void checkType() {
-//    Log.info("###checkType");
-    if (conf.get(TYPE).equals("node")) {
-      this.node = true;
+  /**
+   * Test the type of a CSV input (node or edge)
+   *
+   * @return true if node input
+   */
+  private boolean isNodeCSV() {
+    return conf.get(TYPE).equals(NODE_TYPE);
+  }
+
+  /**
+   * Sets the size of the initial vertex list
+   *
+   * @return vertex list
+   */
+  private List<Vertex> setVList() {
+    int size = conf.getInt(EXPECTED_SIZE, DEFAULT_EXPECTED_SIZE);
+    if (size == 0) {
+      return Lists.newArrayList();
+    } else {
+      return Lists.newArrayListWithExpectedSize(size);
     }
-//    Log.info(node);
   }
 
-  private Vertex readRemainingLines(String line) {
-//    Log.info("###ReadRemainingLines");
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Vertex readVertex(String line) {
     String[] tokens = getTokens(line);
     long id = Long.parseLong(tokens[0]);
-//    Log.info(id);
-//    Log.info(labels.get(0));
     Vertex vex = VertexFactory.createDefaultVertexWithLabels(id, labels, null);
-    if (node) {
-      for (int i = 1; i < properties.length; i++) {
-        switch (types[i]) {
-        case "long":
-          vex.addProperty(properties[i], Long.parseLong(tokens[i]));
-//          Log.info(tokens[i]);
-          break;
-        case "string":
-          vex.addProperty(properties[i], tokens[i]);
-//          Log.info(tokens[i]);
-          break;
-        default:
-          vex.addProperty(properties[i], tokens[i]);
-          break;
-        }
+    for (int i = 1; i < properties.length; i++) {
+      switch (types[i]) {
+      case "long":
+        vex.addProperty(properties[i], Long.parseLong(tokens[i]));
+        break;
+      case "string":
+        vex.addProperty(properties[i], tokens[i]);
+        break;
+      case "integer":
+        vex.addProperty(properties[i], Integer.parseInt(tokens[i]));
+        break;
+      default:
+        vex.addProperty(properties[i], tokens[i]);
+        break;
       }
-    } else {
-      //If edge
     }
     return vex;
   }
 
-  @Override
-  public Vertex readVertex(String line) {
-    return null;
-  }
-
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public List<Vertex> readVertexList(String line) {
-    if (once) {
-      initializeAndReadFirstLine(line);
+    List<Vertex> vList = setVList();
+    if (initialStep) {
+      initialStep(line);
     } else {
-      vList.add(readRemainingLines(line));
+      if (isNodeCSV) {
+        vList.add(readVertex(line));
+      } else {
+        readEdges(vList, line);
+      }
     }
     return vList;
   }
 
-  @Override
-  public boolean supportsVertexLists() {
-    return false;
+  /**
+   * Creates vertices and edges
+   *
+   * @param vertices vertex list
+   * @param line     line of csv input
+   */
+  private void readEdges(List<Vertex> vertices, String line) {
+    List<String> label0 = Lists.newArrayListWithExpectedSize(1);
+    List<String> label1 = Lists.newArrayListWithExpectedSize(1);
+    String[] tokens = getTokens(line);
+    long id0 = Long.parseLong(tokens[0]);
+    long id1 = Long.parseLong(tokens[1]);
+    String label = conf.get(LABEL);
+    label0.add(properties[0].replace(".id", ""));
+    label1.add(properties[1].replace(".id", ""));
+    Edge outgoing =
+      EdgeFactory.createDefaultEdgeWithLabel(id1, label, random.nextLong());
+    Edge incoming =
+      EdgeFactory.createDefaultEdgeWithLabel(id0, label, random.nextLong());
+    for (int i = 2; i < properties.length; i++) {
+      switch (types[i]) {
+      case "long":
+        outgoing.addProperty(properties[i], Long.parseLong(tokens[i]));
+        incoming.addProperty(properties[i], Long.parseLong(tokens[i]));
+        break;
+      case "string":
+        outgoing.addProperty(properties[i], tokens[i]);
+        incoming.addProperty(properties[i], tokens[i]);
+        break;
+      case "integer":
+        outgoing.addProperty(properties[i], Integer.parseInt(tokens[i]));
+        incoming.addProperty(properties[i], Integer.parseInt(tokens[i]));
+        break;
+      default:
+        outgoing.addProperty(properties[i], tokens[i]);
+        incoming.addProperty(properties[i], tokens[i]);
+        break;
+      }
+    }
+    List<Edge> outgoingEdgeList = Lists.newArrayListWithExpectedSize(1);
+    outgoingEdgeList.add(outgoing);
+    List<Edge> incomingEdgeList = Lists.newArrayListWithCapacity(1);
+    incomingEdgeList.add(incoming);
+    Vertex vex0 = VertexFactory
+      .createDefaultVertex(id0, label0, null, outgoingEdgeList, null, null);
+    Vertex vex1 = VertexFactory
+      .createDefaultVertex(id1, label1, null, null, incomingEdgeList, null);
+    vertices.add(vex0);
+    vertices.add(vex1);
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean supportsVertexLists() {
+    return true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public void setConf(Configuration conf) {
     this.conf = conf;
--- gradoop-examples/src/test/java/csv/io/reader/CSVReaderTest.java
+++ gradoop-examples/src/test/java/csv/io/reader/CSVReaderTest.java
@@ -13,55 +13,81 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for {@link org.gradoop.csv.io.reader.CSVReader}.
  */
-public class CSVReaderTest extends GradoopClusterTest {
-  Logger Log = Logger.getLogger(CSVReaderTest.class);
+public class CSVReaderTest{
   private final String NODE_META =
     getClass().getResource("/node_meta.csv").getPath();
-
   private final String EDGE_META =
     getClass().getResource("/edge_meta.csv").getPath();
-
-  private static final long lgid = 0;
   private static final String label_node = "Person";
-  private static final String label_edge = "knows";
-  private static final String firstName = "firstName";
-  private static final String firstName0 = "Arun";
-  private static final String lastName = "lastName";
-  private static final String lastName0 = "Reddy";
-
-  private static final String[] PERSON_CSV = new String[] {
-    "id|firstName|lastName|gender|birthday|creationDate|locationIP" +
-      "|browserUsed|",
-    "0|Arun|Reddy|female|1987-05-27|2010-03-22T12:42:14.255+0000|59.185.111" +
-      ".183|Firefox|",
-    "1|Yang|Li|male|1984-07-09|2010-03-07T13:58:27.400+0000|14.192.76" +
-      ".41|InternetExplorer|"
-  };
-
-  private static final String[] KNOWS_CSV = new String[] {
-    "Person.id|Person.id|creationDate|",
-    "0|1|2011-11-30T05:58:45.255+0000|",
-    "1|0|2012-04-18T01:53:12.825+0000|"
-  };
-
+  private static final String firstNameProperty = "firstName";
+  private static final String lastNameProperty = "lastName";
+  private static final String genderProperty = "gender";
+  private static final String birthdayProperty = "birthday";
+  private static final long person0 = 0;
+  private static final String firstNameValue0 = "Arun";
+  private static final String lastNameValue0 = "Reddy";
+  private static final String genderValue0 = "female";
+  private static final String birthdayValue0 = "1987-05-27";
+  private static final long person1 = 1;
+  private static final String firstNameValue1 = "Yang";
+  private static final String lastNameValue1 = "Li";
+  private static final String genderValue1 = "male";
+  private static final String birthdayValue1 = "1984-07-09";
+  private static final String edgeLabel = "workedAt";
+  private static final long person2 = 2;
+  private static final long person3 = 3;
+  private static final long person4 = 4;
+  private static final long person5 = 5;
+  private static final String[] PERSON_CSV =
+    new String[]{"id|firstName|lastName|gender|birthday|",
+                 "0|Arun|Reddy|female|1987-05-27|",
+                 "1|Yang|Li|male|1984-07-09|"};
+  private static final String[] KNOWS_CSV =
+    new String[]{"Person.id|Organisation.id|since|", "2|3|2015|", "4|5|2012|"};
 
   @Test
   public void checkNodeCSVInputTest() {
-    for (Vertex v : createVerticesFromCSV()) {
+    List<Vertex> vlist = createVerticesFromCSV();
+    assertTrue(vlist.size() == 2);
+    for (Vertex v : vlist) {
       long id = v.getID();
-      if (id == lgid) {
+      if (id == person0) {
         checkLabel(v, label_node);
         for (String propertyKey : v.getPropertyKeys()) {
-          if (propertyKey.equals(firstName)) {
+          if (propertyKey.equals(firstNameProperty)) {
             String value = (String) v.getProperty(propertyKey);
-            assertEquals(value, firstName0);
-          } else if (propertyKey.equals(lastName)) {
-            String value = (String) v.getProperty(lastName);
-            assertEquals(value, lastName0);
+            assertEquals(value, firstNameValue0);
+          } else if (propertyKey.equals(lastNameProperty)) {
+            String value = (String) v.getProperty(lastNameProperty);
+            assertEquals(value, lastNameValue0);
+          } else if (propertyKey.equals(genderProperty)) {
+            String value = (String) v.getProperty(genderProperty);
+            assertEquals(value, genderValue0);
+          } else if (propertyKey.equals(birthdayProperty)) {
+            String value = (String) v.getProperty(birthdayProperty);
+            assertEquals(value, birthdayValue0);
+          }
+        }
+      } else if (id == person1) {
+        checkLabel(v, label_node);
+        for (String propertyKey : v.getPropertyKeys()) {
+          if (propertyKey.equals(firstNameProperty)) {
+            String value = (String) v.getProperty(propertyKey);
+            assertEquals(value, firstNameValue1);
+          } else if (propertyKey.equals(lastNameProperty)) {
+            String value = (String) v.getProperty(lastNameProperty);
+            assertEquals(value, lastNameValue1);
+          } else if (propertyKey.equals(genderProperty)) {
+            String value = (String) v.getProperty(genderProperty);
+            assertEquals(value, genderValue1);
+          } else if (propertyKey.equals(birthdayProperty)) {
+            String value = (String) v.getProperty(birthdayProperty);
+            assertEquals(value, birthdayValue1);
           }
         }
       }
@@ -70,10 +96,27 @@ public class CSVReaderTest extends GradoopClusterTest {
 
   @Test
   public void checkEdgeCSVInputTest() {
-    for (Vertex v : createEdgesFromCSV()) {
-      Log.info("####ID: " + v.getID());
-      Log.info("#####imcomingDegree: " + v.getIncomingDegree());
-      Log.info("#####outgoingdegree: " + v.getOutgoingDegree());
+    List<Vertex> vlist = createEdgesFromCSV();
+    for (Vertex v : vlist) {
+      long id = v.getID();
+      assertEquals(vlist.size(), 4);
+      if (id == person2) {
+        assertEquals(v.getOutgoingDegree(), 1);
+        assertEquals(v.getIncomingDegree(), 0);
+        checkOutgoingEdge(Lists.newArrayList(v.getOutgoingEdges()));
+      } else if (id == person3) {
+        assertEquals(v.getIncomingDegree(), 1);
+        assertEquals(v.getOutgoingDegree(), 0);
+        checkIncomingEdge(Lists.newArrayList(v.getIncomingEdges()));
+      } else if (id == person4) {
+        assertEquals(v.getIncomingDegree(), 0);
+        assertEquals(v.getOutgoingDegree(), 1);
+        checkOutgoingEdge(Lists.newArrayList(v.getOutgoingEdges()));
+      } else if (id == person5) {
+        assertEquals(v.getIncomingDegree(), 1);
+        assertEquals(v.getOutgoingDegree(), 0);
+        checkIncomingEdge(Lists.newArrayList(v.getIncomingEdges()));
+      }
     }
   }
 
@@ -85,7 +128,13 @@ public class CSVReaderTest extends GradoopClusterTest {
 
   protected void checkOutgoingEdge(List<Edge> edges) {
     for (Edge edge : edges) {
-      assertEquals(edge.getOtherID(), "1");
+      assertEquals(edge.getLabel(), edgeLabel);
+    }
+  }
+
+  protected void checkIncomingEdge(List<Edge> edges) {
+    for (Edge edge : edges) {
+      assertEquals(edge.getLabel(), edgeLabel);
     }
   }
 
@@ -98,7 +147,8 @@ public class CSVReaderTest extends GradoopClusterTest {
     reader.setConf(conf);
     List<Vertex> vertices = Lists.newArrayList();
     for (String line : PERSON_CSV) {
-      for (Vertex v : reader.readVertexList(line)) {
+      List<Vertex> verticesPerLine = reader.readVertexList(line);
+      for (Vertex v : verticesPerLine) {
         vertices.add(v);
       }
     }
@@ -109,7 +159,7 @@ public class CSVReaderTest extends GradoopClusterTest {
     ConfigurableVertexLineReader reader = new CSVReader();
     Configuration conf = new Configuration();
     conf.set(CSVReader.META_DATA, EDGE_META);
-    conf.set(CSVReader.LABEL, "knows");
+    conf.set(CSVReader.LABEL, "workedAt");
     conf.set(CSVReader.TYPE, "edge");
     reader.setConf(conf);
     List<Vertex> vertices = Lists.newArrayList();
