b2804f9740d58d64e71b097f8b28050057258a70
Martin Junghanns <martin.junghanns@gmx.net>
2015-06-18 14:15:17 +0200

    updated copyright

--- gradoop-core/src/main/java/org/gradoop/drivers/BulkDriver.java
+++ gradoop-core/src/main/java/org/gradoop/drivers/BulkDriver.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.gradoop.drivers;
 
 import com.google.common.base.Splitter;
@@ -224,8 +241,7 @@ public abstract class BulkDriver extends Configured implements Tool {
         LOG.error("No arguments were provided (try -h)");
       }
       CommandLineParser parser = new BasicParser();
-      CommandLine cmd = parser.parse(OPTIONS, args, false);
-      return cmd;
+      return parser.parse(OPTIONS, args, false);
     }
   }
 }
