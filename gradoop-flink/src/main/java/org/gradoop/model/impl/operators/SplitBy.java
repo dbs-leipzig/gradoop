02d85d1bf18c82a930b50fec3227e17c670d5582
Niklas Teichmann <niklasteichmann42@gmail.com>
2015-08-18 14:07:26 +0200

    #49 restructured SplitBy class
    created methods  computeNewVertices(), computeNewSubgraphs() and  computeNewEdges()
    to improve legibility
    
    created new static inner classes for each operator

--- gradoop-flink/src/main/java/org/gradoop/model/impl/operators/SplitBy.java
+++ gradoop-flink/src/main/java/org/gradoop/model/impl/operators/SplitBy.java
@@ -76,132 +76,76 @@ public class SplitBy<VD extends VertexData, ED extends EdgeData, GD extends
    * {@inheritDoc}
    * executes the SplitBy operation on a graph
    *
-   * @param epGraph EPGraph
+   * @param logicalGraph EPGraph
    */
   @Override
-  public GraphCollection<VD, ED, GD> execute(LogicalGraph<VD, ED, GD> epGraph) {
+  public GraphCollection<VD, ED, GD> execute(
+    LogicalGraph<VD, ED, GD> logicalGraph) {
+    DataSet<Vertex<Long, VD>> vertices = computeNewVertices(logicalGraph);
+    DataSet<Subgraph<Long, GD>> subgraphs =
+      computeNewSubgraphs(logicalGraph, vertices);
+    DataSet<Edge<Long, ED>> edges =
+      computeNewEdges(logicalGraph, vertices, subgraphs);
+    Graph<Long, VD, ED> newGraph = Graph.fromDataSet(vertices, edges, env);
+    return new GraphCollection<>(newGraph, subgraphs,
+      logicalGraph.getVertexDataFactory(), logicalGraph.getEdgeDataFactory(),
+      logicalGraph.getGraphDataFactory(), env);
+  }
+
+  private DataSet<Vertex<Long, VD>> computeNewVertices(
+    LogicalGraph<VD, ED, GD> logicalGraph) {
+    //get the Gelly graph and vertices
+    final Graph<Long, VD, ED> graph = logicalGraph.getGellyGraph();
+    DataSet<Vertex<Long, VD>> vertices = graph.getVertices();
+    //add the new graphs to the vertices graph lists
+    return vertices.map(new AddNewGraphsToVertexMapper<>(function));
+  }
+
+  private DataSet<Subgraph<Long, GD>> computeNewSubgraphs(
+    LogicalGraph<VD, ED, GD> logicalGraph, DataSet<Vertex<Long, VD>> vertices) {
     // construct a KeySelector using the LongFromVertexFunction
     KeySelector<Vertex<Long, VD>, Long> propertySelector =
       new LongFromVertexSelector<>(function);
-    //get the Gelly graph and vertices
-    final Graph<Long, VD, ED> graph = epGraph.getGellyGraph();
-    DataSet<Vertex<Long, VD>> vertices = graph.getVertices();
-    //add the new graphs to the vertices graph lists
-    vertices = vertices.map(new AddNewGraphsToVertexMapper<>(function));
     //construct the list of subgraphs
-    GraphDataFactory<GD> gdFactory = epGraph.getGraphDataFactory();
-    DataSet<Subgraph<Long, GD>> subgraphs = vertices.groupBy(propertySelector)
+    GraphDataFactory<GD> gdFactory = logicalGraph.getGraphDataFactory();
+    return vertices.groupBy(propertySelector)
       .reduceGroup(new SubgraphsFromGroupsReducer<>(function, gdFactory));
+  }
+
+  private DataSet<Edge<Long, ED>> computeNewEdges(
+    LogicalGraph<VD, ED, GD> logicalGraph, DataSet<Vertex<Long, VD>> vertices,
+    DataSet<Subgraph<Long, GD>> subgraphs) {
+    final Graph<Long, VD, ED> graph = logicalGraph.getGellyGraph();
     //construct tuples of the edges with the ids of their source and target
     // vertices
-    DataSet<Tuple3<Long, Long, Long>> edgeVertexVertex = graph.getEdges()
-      .map(new MapFunction<Edge<Long, ED>, Tuple3<Long, Long, Long>>() {
-        @Override
-        public Tuple3<Long, Long, Long> map(Edge<Long, ED> edge) throws
-          Exception {
-          return new Tuple3<>(edge.getValue().getId(),
-            edge.getValue().getSourceVertexId(),
-            edge.getValue().getTargetVertexId());
-        }
-      });
+    DataSet<Tuple3<Long, Long, Long>> edgeVertexVertex =
+      graph.getEdges().map(new EdgeToTupleMapper<ED>());
     //replace the source vertex id by the graph list of this vertex
     DataSet<Tuple3<Long, Set<Long>, Long>> edgeGraphsVertex =
-      edgeVertexVertex.join(vertices).where(1).equalTo(0).with(
-        new JoinFunction<Tuple3<Long, Long, Long>, Vertex<Long, VD>,
-          Tuple3<Long, Set<Long>, Long>>() {
-          @Override
-          public Tuple3<Long, Set<Long>, Long> join(
-            Tuple3<Long, Long, Long> tuple3, Vertex<Long, VD> vertex) throws
-            Exception {
-            return new Tuple3<>(tuple3.f0, vertex.getValue().getGraphs(),
-              tuple3.f2);
-          }
-        });
+      edgeVertexVertex.join(vertices).where(1).equalTo(0)
+        .with(new JoinEdgeTupleWithSourceGraphs<VD>());
     //replace the target vertex id by the graph list of this vertex
     DataSet<Tuple3<Long, Set<Long>, Set<Long>>> edgeGraphsGraphs =
-      edgeGraphsVertex.join(vertices).where(2).equalTo(0).with(
-        new JoinFunction<Tuple3<Long, Set<Long>, Long>, Vertex<Long, VD>,
-          Tuple3<Long, Set<Long>, Set<Long>>>() {
-          @Override
-          public Tuple3<Long, Set<Long>, Set<Long>> join(
-            Tuple3<Long, Set<Long>, Long> tuple3,
-            Vertex<Long, VD> vertex) throws Exception {
-            return new Tuple3<>(tuple3.f0, tuple3.f1,
-              vertex.getValue().getGraphs());
-          }
-        });
+      edgeGraphsVertex.join(vertices).where(2).equalTo(0)
+        .with(new JoinEdgeTupleWithTargetGraphs<VD>());
     //transform the new subgraphs into a single set of long, containing all
     // the identifiers
     DataSet<Set<Long>> newSubgraphIdentifiers =
-      subgraphs.map(new MapFunction<Subgraph<Long, GD>, Set<Long>>() {
-        @Override
-        public Set<Long> map(Subgraph<Long, GD> subgraph) throws Exception {
-          Set<Long> id = Sets.newHashSet();
-          id.add(subgraph.getId());
-          return id;
-        }
-      }).reduce(new ReduceFunction<Set<Long>>() {
-        @Override
-        public Set<Long> reduce(Set<Long> set1, Set<Long> set2) throws
-          Exception {
-          set1.addAll(set2);
-          return set1;
-        }
-      });
+      subgraphs.map(new MapSubgraphIdToSet<GD>()).reduce(new ReduceSets());
     //construct new tuples containing the edge, the graphs of its source and
     //target vertex and the list of new graphs
     DataSet<Tuple4<Long, Set<Long>, Set<Long>, Set<Long>>> edgesWithSubgraphs =
-      edgeGraphsGraphs.crossWithTiny(newSubgraphIdentifiers).with(
-        new CrossFunction<Tuple3<Long, Set<Long>, Set<Long>>, Set<Long>,
-          Tuple4<Long, Set<Long>, Set<Long>, Set<Long>>>() {
-          @Override
-          public Tuple4<Long, Set<Long>, Set<Long>, Set<Long>> cross(
-            Tuple3<Long, Set<Long>, Set<Long>> tuple3,
-            Set<Long> subgraphs) throws Exception {
-            return new Tuple4<>(tuple3.f0, tuple3.f1, tuple3.f2, subgraphs);
-          }
-        });
+      edgeGraphsGraphs.crossWithTiny(newSubgraphIdentifiers)
+        .with(new CrossEdgesWithGraphSet());
     //remove all edges which source and target are not in at least one common
     // graph
-    DataSet<Tuple2<Long, Set<Long>>> newSubgraphs = edgesWithSubgraphs.flatMap(
-      new FlatMapFunction<Tuple4<Long, Set<Long>, Set<Long>, Set<Long>>,
-        Tuple2<Long, Set<Long>>>() {
-        @Override
-        public void flatMap(
-          Tuple4<Long, Set<Long>, Set<Long>, Set<Long>> tuple4,
-          Collector<Tuple2<Long, Set<Long>>> collector) throws Exception {
-          Set<Long> sourceGraphs = tuple4.f1;
-          Set<Long> targetGraphs = tuple4.f2;
-          Set<Long> newSubgraphs = tuple4.f3;
-          boolean newGraphAdded = false;
-          Set<Long> toBeAddedGraphs = new HashSet<Long>();
-          for (Long graph : newSubgraphs) {
-            if (targetGraphs.contains(graph) && sourceGraphs.contains(graph)) {
-              toBeAddedGraphs.add(graph);
-              newGraphAdded = true;
-            }
-          }
-          if (newGraphAdded) {
-            collector.collect(new Tuple2<>(tuple4.f0, toBeAddedGraphs));
-          }
-        }
-      });
+    DataSet<Tuple2<Long, Set<Long>>> newSubgraphs =
+      edgesWithSubgraphs.flatMap(new CheckEdgesSourceTargetGraphs());
     //join the graph set tuples with the edges, add all new graphs to the
     //edge graph sets
-    DataSet<Edge<Long, ED>> edges = graph.getEdges().join(newSubgraphs)
-      .where(new KeySelectors.EdgeKeySelector<ED>()).equalTo(0).with(
-        new JoinFunction<Edge<Long, ED>, Tuple2<Long, Set<Long>>, Edge<Long,
-          ED>>() {
-          @Override
-          public Edge<Long, ED> join(Edge<Long, ED> edge,
-            Tuple2<Long, Set<Long>> tuple2) throws Exception {
-            return edge;
-          }
-        });
-    Graph<Long, VD, ED> newGraph = Graph.fromDataSet(vertices, edges, env);
-    return new GraphCollection<>(newGraph, subgraphs,
-      epGraph.getVertexDataFactory(), epGraph.getEdgeDataFactory(),
-      epGraph.getGraphDataFactory(), env);
+    return graph.getEdges().join(newSubgraphs)
+      .where(new KeySelectors.EdgeKeySelector<ED>()).equalTo(0)
+      .with(new JoinEdgeTuplesWithEdges<ED>());
   }
 
   @Override
@@ -211,6 +155,8 @@ public class SplitBy<VD extends VertexData, ED extends EdgeData, GD extends
 
   /**
    * applies the LongFromVertexFunction on a vertex
+   *
+   * @param <VD> vertex data
    */
   private static class LongFromVertexSelector<VD extends VertexData> implements
     KeySelector<Vertex<Long, VD>, Long> {
@@ -241,6 +187,8 @@ public class SplitBy<VD extends VertexData, ED extends EdgeData, GD extends
   /**
    * adds the graph ids extracted by the LongFromVertexFunction to the
    * vertex graph set
+   *
+   * @param <VD> vertex data
    */
   private static class AddNewGraphsToVertexMapper<VD extends VertexData>
     implements
@@ -273,6 +221,9 @@ public class SplitBy<VD extends VertexData, ED extends EdgeData, GD extends
 
   /**
    * builds new graphs from vertices and the LongFromVertexFunction
+   *
+   * @param <VD> vertex data
+   * @param <GD> graph data
    */
   private static class SubgraphsFromGroupsReducer<VD extends VertexData, GD
     extends GraphData> implements
@@ -323,6 +274,163 @@ public class SplitBy<VD extends VertexData, ED extends EdgeData, GD extends
     public TypeInformation<Vertex<Long, VD>> getProducedType() {
       return new TupleTypeInfo(Subgraph.class, BasicTypeInfo.LONG_TYPE_INFO,
         TypeExtractor.createTypeInfo(graphDataFactory.getType()));
+    }
+  }
+
+  /**
+   * maps the edges to tuples containing the edge id and the ids of its
+   * source and target vertices
+   *
+   * @param <ED> edge data
+   */
+  private static class EdgeToTupleMapper<ED extends EdgeData> implements
+    MapFunction<Edge<Long, ED>, Tuple3<Long, Long, Long>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuple3<Long, Long, Long> map(Edge<Long, ED> edge) throws Exception {
+      return new Tuple3<>(edge.getValue().getId(),
+        edge.getValue().getSourceVertexId(),
+        edge.getValue().getTargetVertexId());
+    }
+  }
+
+  /**
+   * replace the id of the source vertex of each edge with the set of graphs
+   * this vertex belongs to
+   *
+   * @param <VD> vertex data
+   */
+  private static class JoinEdgeTupleWithSourceGraphs<VD extends VertexData>
+    implements
+    JoinFunction<Tuple3<Long, Long, Long>, Vertex<Long, VD>, Tuple3<Long,
+      Set<Long>, Long>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuple3<Long, Set<Long>, Long> join(Tuple3<Long, Long, Long> tuple3,
+      Vertex<Long, VD> vertex) throws Exception {
+      return new Tuple3<>(tuple3.f0, vertex.getValue().getGraphs(), tuple3.f2);
+    }
+  }
+
+  /**
+   * replace the id of the target vertex of each edge with the set of graphs
+   * this vertex belongs to
+   *
+   * @param <VD> vertex data
+   */
+  private static class JoinEdgeTupleWithTargetGraphs<VD extends VertexData>
+    implements
+    JoinFunction<Tuple3<Long, Set<Long>, Long>, Vertex<Long, VD>,
+      Tuple3<Long, Set<Long>, Set<Long>>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuple3<Long, Set<Long>, Set<Long>> join(
+      Tuple3<Long, Set<Long>, Long> tuple3, Vertex<Long, VD> vertex) throws
+      Exception {
+      return new Tuple3<>(tuple3.f0, tuple3.f1, vertex.getValue().getGraphs());
+    }
+  }
+
+  /**
+   * extract the id of each subgraph and put it into a Set<Long>
+   *
+   * @param <GD> graph data
+   */
+  private static class MapSubgraphIdToSet<GD extends GraphData> implements
+    MapFunction<Subgraph<Long, GD>, Set<Long>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<Long> map(Subgraph<Long, GD> subgraph) throws Exception {
+      Set<Long> id = Sets.newHashSet();
+      id.add(subgraph.getId());
+      return id;
+    }
+  }
+
+  /**
+   * merge two sets into one by adding the contents of both into a new set
+   */
+  private static class ReduceSets implements ReduceFunction<Set<Long>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<Long> reduce(Set<Long> set1, Set<Long> set2) throws Exception {
+      set1.addAll(set2);
+      return set1;
+    }
+  }
+
+  /**
+   * cross the edge tuples with a set containing the ids of the new graphs
+   */
+  private static class CrossEdgesWithGraphSet implements
+    CrossFunction<Tuple3<Long, Set<Long>, Set<Long>>, Set<Long>, Tuple4<Long,
+      Set<Long>, Set<Long>, Set<Long>>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuple4<Long, Set<Long>, Set<Long>, Set<Long>> cross(
+      Tuple3<Long, Set<Long>, Set<Long>> tuple3, Set<Long> subgraphs) throws
+      Exception {
+      return new Tuple4<>(tuple3.f0, tuple3.f1, tuple3.f2, subgraphs);
+    }
+  }
+
+  /**
+   * check for each edge if its source and target vertices are in the same
+   * new graph
+   */
+  private static class CheckEdgesSourceTargetGraphs implements
+    FlatMapFunction<Tuple4<Long, Set<Long>, Set<Long>, Set<Long>>,
+      Tuple2<Long, Set<Long>>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void flatMap(Tuple4<Long, Set<Long>, Set<Long>, Set<Long>> tuple4,
+      Collector<Tuple2<Long, Set<Long>>> collector) throws Exception {
+      Set<Long> sourceGraphs = tuple4.f1;
+      Set<Long> targetGraphs = tuple4.f2;
+      Set<Long> newSubgraphs = tuple4.f3;
+      boolean newGraphAdded = false;
+      Set<Long> toBeAddedGraphs = new HashSet<>();
+      for (Long graph : newSubgraphs) {
+        if (targetGraphs.contains(graph) && sourceGraphs.contains(graph)) {
+          toBeAddedGraphs.add(graph);
+          newGraphAdded = true;
+        }
+      }
+      if (newGraphAdded) {
+        collector.collect(new Tuple2<>(tuple4.f0, toBeAddedGraphs));
+      }
+    }
+  }
+
+  /**
+   * join the edge tuples with the edges, add the new graphs to the graph set
+   *
+   * @param <ED> edge data
+   */
+  private static class JoinEdgeTuplesWithEdges<ED extends EdgeData> implements
+    JoinFunction<Edge<Long, ED>, Tuple2<Long, Set<Long>>, Edge<Long, ED>> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Edge<Long, ED> join(Edge<Long, ED> edge,
+      Tuple2<Long, Set<Long>> tuple2) throws Exception {
+      edge.getValue().getGraphs().addAll(tuple2.f1);
+      return edge;
     }
   }
 }
