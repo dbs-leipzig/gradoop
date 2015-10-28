efe48cebe2c16f897246a7e46ffb73d971cddb66
niklas <niklasteichmann42@gmail.com>
2015-10-28 12:56:23 +0100

    implemented ExcludeCollection operator, fixes #89

---
+++ gradoop-flink/src/main/java/org/gradoop/model/impl/functions/filterfunctions/EdgeInNoneOfGraphsFilterWithBC.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of gradoop.
+ *
+ * gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gradoop.model.impl.functions.filterfunctions;
+
+import org.apache.flink.api.common.functions.RichFilterFunction;
+import org.apache.flink.configuration.Configuration;
+import org.gradoop.model.api.EdgeData;
+
+import java.util.List;
+
+/**
+ * Checks if an edge is contained in all of the given logical
+ * graphs.
+ * <p/>
+ * Graph identifiers are read from a broadcast set.
+ *
+ * @param <ED> EPGM edge type
+ */
+public class EdgeInNoneOfGraphsFilterWithBC<ED extends EdgeData> extends
+  RichFilterFunction<ED> {
+  /**
+   * Name of the broadcast variable which is accessed by this function.
+   */
+  public static final String BC_IDENTIFIERS = "bc.identifiers";
+  /**
+   * Graph identifiers
+   */
+  private List<Long> identifiers;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void open(Configuration parameters) throws Exception {
+    identifiers = getRuntimeContext().getBroadcastVariable(BC_IDENTIFIERS);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean filter(ED edge) throws Exception {
+    boolean vertexInGraph = true;
+    if (edge.getGraphCount() > 0) {
+      for (Long graph : identifiers) {
+        if (edge.getGraphs().contains(graph)) {
+          vertexInGraph = false;
+          break;
+        }
+      }
+    }
+    return vertexInGraph;
+  }
+}
---
+++ gradoop-flink/src/main/java/org/gradoop/model/impl/functions/filterfunctions/VertexInNoneOfGraphsFilterWithBC.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of gradoop.
+ *
+ * gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gradoop.model.impl.functions.filterfunctions;
+
+import org.apache.flink.api.common.functions.RichFilterFunction;
+import org.apache.flink.configuration.Configuration;
+import org.gradoop.model.api.VertexData;
+
+import java.util.List;
+
+/**
+ * Checks if a vertex is contained in all of the given logical
+ * graphs.
+ * <p/>
+ * Graph identifiers are read from a broadcast set.
+ *
+ * @param <VD> EPGM vertex type
+ */
+public class VertexInNoneOfGraphsFilterWithBC<VD extends VertexData> extends
+  RichFilterFunction<VD> {
+  /**
+   * Name of the broadcast variable which is accessed by this function.
+   */
+  public static final String BC_IDENTIFIERS = "bc.identifiers";
+  /**
+   * Graph identifiers
+   */
+  private List<Long> identifiers;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void open(Configuration parameters) throws Exception {
+    identifiers = getRuntimeContext().getBroadcastVariable(BC_IDENTIFIERS);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean filter(VD vertex) throws Exception {
+    boolean vertexInGraph = true;
+    if (vertex.getGraphCount() > 0) {
+      for (Long graph : identifiers) {
+        if (vertex.getGraphs().contains(graph)) {
+          vertexInGraph = false;
+          break;
+        }
+      }
+    }
+    return vertexInGraph;
+  }
+}
+
