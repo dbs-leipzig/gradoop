ce5f2ec54eb90caf6e7a10c4c3bf908dc222f096
p3et <petermann@informatik.uni-leipzig.de>
2015-11-20 17:36:57 +0100

    too few memory segments error workaround, #102

--- gradoop-flink/src/test/java/org/gradoop/model/FlinkTestBase.java
+++ gradoop-flink/src/test/java/org/gradoop/model/FlinkTestBase.java
@@ -18,12 +18,22 @@
 package org.gradoop.model;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.configuration.ConfigConstants;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.StreamingMode;
+import org.apache.flink.test.util.ForkableFlinkMiniCluster;
 import org.apache.flink.test.util.MultipleProgramsTestBase;
 import org.gradoop.GradoopTestBaseUtils;
 import org.gradoop.model.impl.pojo.EdgePojo;
 import org.gradoop.model.impl.pojo.GraphHeadPojo;
 import org.gradoop.model.impl.pojo.VertexPojo;
 import org.gradoop.model.impl.EPGMDatabase;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Used for tests that require a Flink cluster up and running.
@@ -63,4 +73,53 @@ public class FlinkTestBase extends MultipleProgramsTestBase {
   protected ExecutionEnvironment getExecutionEnvironment() {
     return env;
   }
+
+  /**
+   * Custom test cluster start routine,
+   * workaround to set TASK_MANAGER_MEMORY_SIZE.
+   *
+   * @throws Exception
+   */
+  @BeforeClass
+  public static void setup() throws Exception {
+
+    logDir = File.createTempFile("TestBaseUtils-logdir", null);
+    Assert.assertTrue("Unable to delete temp file", logDir.delete());
+    Assert.assertTrue("Unable to create temp directory", logDir.mkdir());
+    Path logFile = Files.createFile(new File(logDir, "jobmanager.log").toPath());
+    Files.createFile(new File(logDir, "jobmanager.out").toPath());
+
+    Configuration config = new Configuration();
+
+    config.setInteger(
+      ConfigConstants.LOCAL_NUMBER_TASK_MANAGER, 1);
+    config.setInteger(
+      ConfigConstants.TASK_MANAGER_NUM_TASK_SLOTS, 4);
+
+    config.setBoolean(
+      ConfigConstants.LOCAL_START_WEBSERVER, startWebServer);
+
+    config.setLong(
+      ConfigConstants.TASK_MANAGER_MEMORY_SIZE_KEY, 128L);
+    config.setBoolean(
+      ConfigConstants.FILESYSTEM_DEFAULT_OVERWRITE_KEY, true);
+
+    config.setString(
+      ConfigConstants.AKKA_ASK_TIMEOUT, DEFAULT_AKKA_ASK_TIMEOUT + "s");
+    config.setString(
+      ConfigConstants.AKKA_STARTUP_TIMEOUT, DEFAULT_AKKA_STARTUP_TIMEOUT);
+
+    config.setInteger(
+      ConfigConstants.JOB_MANAGER_WEB_PORT_KEY, 8081);
+    config.setString(
+      ConfigConstants.JOB_MANAGER_WEB_LOG_PATH_KEY, logFile.toString());
+
+    ForkableFlinkMiniCluster cluster =
+      new ForkableFlinkMiniCluster(config, true, StreamingMode.BATCH_ONLY);
+
+    cluster.start();
+
+    MultipleProgramsTestBase.cluster = cluster;
+  }
+
 }
