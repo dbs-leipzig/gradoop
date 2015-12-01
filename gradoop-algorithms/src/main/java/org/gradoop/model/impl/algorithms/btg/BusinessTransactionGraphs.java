60cd5b31576bc467333d8812b587de502ebd1037
p3et <petermann@informatik.uni-leipzig.de>
2015-12-01 17:02:32 +0100

    new BTG algorithm added  #102

---
+++ gradoop-algorithms/src/main/java/org/gradoop/model/impl/algorithms/btg/BusinessTransactionGraphs.java
@@ -0,0 +1,33 @@
+package org.gradoop.model.impl.algorithms.btg;
+
+import org.gradoop.model.api.EPGMEdge;
+import org.gradoop.model.api.EPGMGraphHead;
+import org.gradoop.model.api.EPGMVertex;
+import org.gradoop.model.api.operators.UnaryGraphToCollectionOperator;
+import org.gradoop.model.impl.GraphCollection;
+import org.gradoop.model.impl.LogicalGraph;
+import org.gradoop.model.impl.pojo.EdgePojo;
+import org.gradoop.model.impl.pojo.GraphHeadPojo;
+import org.gradoop.model.impl.pojo.VertexPojo;
+
+public class BusinessTransactionGraphs
+  <G extends EPGMGraphHead, V extends EPGMVertex, E extends EPGMEdge>
+  implements UnaryGraphToCollectionOperator<G, V, E> {
+  public static final String SUPERCLASS_KEY = "btgsc";
+  public static final String MASTERDATA_VALUE = "M";
+  public static final String TRANSDATA_VALUE = "T";
+
+  @Override
+  public GraphCollection<G, V, E>
+  execute(LogicalGraph<G, V, E> graph) throws Exception {
+
+    return GraphCollection.createEmptyCollection(graph.getConfig());
+  }
+
+  @Override
+  public String getName() {
+    return this.getClass().getSimpleName();
+  }
+
+
+}
