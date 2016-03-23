2ad0e42ac69aff4cd593c800dbf2c30d338ce0df
p3et <petermann@informatik.uni-leipzig.de>
2016-03-23 15:55:57 +0100

    foodbroker porting started, #162

---
+++ gradoop-datagen/src/test/java/org/gradoop/model/impl/datagen/foodbroker/FoodBrokerTest.java
@@ -0,0 +1,26 @@
+package org.gradoop.model.impl.datagen.foodbroker;
+
+import org.gradoop.model.GradoopFlinkTestBase;
+import org.gradoop.model.impl.LogicalGraph;
+import org.gradoop.model.impl.datagen.FoodBroker;
+import org.gradoop.model.impl.datagen.config.FoodBrokerConfig;
+import org.gradoop.model.impl.pojo.EdgePojo;
+import org.gradoop.model.impl.pojo.GraphHeadPojo;
+import org.gradoop.model.impl.pojo.VertexPojo;
+import org.junit.Test;
+
+public class FoodBrokerTest extends GradoopFlinkTestBase {
+  @Test
+  public void testGenerate() throws Exception {
+
+    FoodBroker<GraphHeadPojo, VertexPojo, EdgePojo> foodBroker =
+      new FoodBroker<>(getExecutionEnvironment(), getConfig(), FoodBrokerConfig
+        .fromFile(FoodBrokerTest.class.getResource("/foodbroker/config.json")
+          .getFile()));
+
+    LogicalGraph<GraphHeadPojo, VertexPojo, EdgePojo> enterpriseGraph =
+      foodBroker.generate(1);
+
+    //GradoopFlinkTestUtils.printLogicalGraph(enterpriseGraph);
+  }
+}
