6b1b9fcca3d771a9057f7c3e57187dd97856041b
p3et <petermann@informatik.uni-leipzig.de>
2016-03-23 15:55:57 +0100

    foodbroker porting started, #162

---
+++ gradoop-datagen/src/main/java/org/gradoop/model/impl/datagen/model/MasterDataSeed.java
@@ -0,0 +1,18 @@
+package org.gradoop.model.impl.datagen.model;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+
+/**
+ * Created by peet on 23.03.16.
+ */
+public class MasterDataSeed extends Tuple2<Long, Short> {
+
+  public MasterDataSeed() {
+
+  }
+
+  public MasterDataSeed(Long longId, Short kind) {
+    this.f0 = longId;
+    this.f1 = kind;
+  }
+}
--- gradoop-datagen/src/test/java/org/gradoop/model/impl/datagen/foodbroker/FoodBrokerTest.java
+++ gradoop-datagen/src/test/java/org/gradoop/model/impl/datagen/foodbroker/FoodBrokerTest.java
@@ -2,7 +2,8 @@ package org.gradoop.model.impl.datagen.foodbroker;
 
 import org.gradoop.model.GradoopFlinkTestBase;
 import org.gradoop.model.impl.LogicalGraph;
-import org.gradoop.model.impl.datagen.foodbroker.config.FoodBrokerConfig;
+import org.gradoop.model.impl.datagen.FoodBroker;
+import org.gradoop.model.impl.datagen.config.FoodBrokerConfig;
 import org.gradoop.model.impl.pojo.EdgePojo;
 import org.gradoop.model.impl.pojo.GraphHeadPojo;
 import org.gradoop.model.impl.pojo.VertexPojo;
@@ -18,7 +19,7 @@ public class FoodBrokerTest extends GradoopFlinkTestBase {
           .getFile()));
 
     LogicalGraph<GraphHeadPojo, VertexPojo, EdgePojo> enterpriseGraph =
-      foodBroker.generate(0);
+      foodBroker.generate(1);
 
     //GradoopFlinkTestUtils.printLogicalGraph(enterpriseGraph);
   }
