dd903bb3db2837406cb47825e11ec185970b30f9
p3et <petermann@informatik.uni-leipzig.de>
2016-03-23 15:55:57 +0100

    foodbroker porting started, #162

---
+++ gradoop-datagen/src/main/java/org/gradoop/model/impl/datagen/masterdata/MasterDataGenerator.java
@@ -0,0 +1,100 @@
+package org.gradoop.model.impl.datagen.masterdata;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.gradoop.model.api.EPGMVertex;
+import org.gradoop.model.api.EPGMVertexFactory;
+import org.gradoop.model.impl.datagen.config.FoodBrokerConfig;
+import org.gradoop.model.impl.datagen.model.MasterDataSeed;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by peet on 23.03.16.
+ */
+public class MasterDataGenerator<V extends EPGMVertex> {
+  public static final Short GOOD_VALUE = 0;
+  public static final Short NORMAL_VALUE = 1;
+  public static final Short BAD_VALUE = 2;
+
+  private final FoodBrokerConfig foodBrokerConfig;
+  protected final ExecutionEnvironment env ;
+  protected final EPGMVertexFactory<V> vertexFactory;
+
+  public MasterDataGenerator(ExecutionEnvironment env,
+    FoodBrokerConfig foodBrokerConfig, EPGMVertexFactory<V> vertexFactory) {
+    this.foodBrokerConfig = foodBrokerConfig;
+    this.env = env;
+    this.vertexFactory = vertexFactory;
+  }
+
+  protected List<MasterDataSeed> getMasterDataSeeds(String className) {
+    Integer offset = foodBrokerConfig
+      .getMasterDataOffset(className);
+
+    Integer growth = foodBrokerConfig
+      .getMasterDataGrowth(className);
+
+    Double goodRatio = foodBrokerConfig
+      .getMasterDataGoodRatio(className);
+
+    Double badRatio = foodBrokerConfig
+      .getMasterDataBadRatio(className);
+
+    Integer count = offset + foodBrokerConfig.getScaleFactor() * growth;
+
+    Long goodCount = Math.round(count * goodRatio);
+    Long badCount = Math.round(count * badRatio);
+    Long normalCount = count - goodCount - badCount;
+
+    List<MasterDataSeed> seedList = new ArrayList<>();
+
+    Long currentId = 0L;
+
+    Map<Short, Long> qualityCounts = new HashMap<>();
+
+    qualityCounts.put(MasterDataGenerator.GOOD_VALUE, goodCount);
+    qualityCounts.put(MasterDataGenerator.NORMAL_VALUE, normalCount);
+    qualityCounts.put(MasterDataGenerator.BAD_VALUE, badCount);
+
+    for(Map.Entry<Short, Long> qualityCount : qualityCounts.entrySet()) {
+
+      Short quality = qualityCount.getKey();
+
+      for(int i = 1; i <= qualityCount.getValue(); i++) {
+        currentId++;
+        seedList.add(new MasterDataSeed(
+          currentId, quality
+        ));
+      }
+    }
+
+    Collections.shuffle(seedList);
+
+    return seedList;
+  }
+
+  protected List<String> getStringValuesFromFile(String fileName) {
+    fileName = "/foodbroker/" + fileName;
+
+
+    List<String> values = null;
+
+    try {
+      String adjectivesPath = CustomerGenerator.class
+        .getResource(fileName).getFile();
+
+      values = FileUtils.readLines(FileUtils.getFile
+        (adjectivesPath));
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return values;
+  }
+}
---
+++ gradoop-datagen/src/main/java/org/gradoop/model/impl/datagen/model/MasterDataObject.java
@@ -0,0 +1,18 @@
+package org.gradoop.model.impl.datagen.model;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.gradoop.model.api.EPGMVertex;
+
+public class MasterDataObject<V extends EPGMVertex>
+  extends Tuple3<Long, Short, V>{
+
+  public MasterDataObject(MasterDataSeed seed, V vertex) {
+    this.f0 = seed.f0;
+    this.f1 = seed.f1;
+    this.f2 = vertex;
+  }
+
+  public Short getQuality() {
+    return this.f1;
+  }
+}
