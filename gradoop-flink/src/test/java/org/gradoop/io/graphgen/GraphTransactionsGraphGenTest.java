990d1000ab9a569d77d1c54502b549824fe55022
Stephan Kemper <stephan.ke@gmx.de>
2016-06-08 13:59:21 +0200

    Added another test method.

--- gradoop-flink/src/test/java/org/gradoop/io/graphgen/GraphTransactionsGraphGenTest.java
+++ gradoop-flink/src/test/java/org/gradoop/io/graphgen/GraphTransactionsGraphGenTest.java
@@ -1,11 +1,13 @@
 package org.gradoop.io.graphgen;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.gradoop.model.GradoopFlinkTestBase;
 import org.gradoop.model.impl.GraphTransactions;
 import org.gradoop.model.impl.pojo.EdgePojo;
 import org.gradoop.model.impl.pojo.GraphHeadPojo;
 import org.gradoop.model.impl.pojo.VertexPojo;
+import org.gradoop.model.impl.tuples.GraphTransaction;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -28,6 +30,27 @@ public class GraphTransactionsGraphGenTest extends GradoopFlinkTestBase {
       GraphTransactions.fromGraphGenFile(graphGenFile, getExecutionEnvironment());
 
     assertEquals("Wrong graph count", 2, graphTransactions.getTransactions().count());
+
+  }
+
+  /**
+   * Test method for
+   * {@link org.gradoop.model.impl.EPGMDatabase#fromGraphGenFile(String, ExecutionEnvironment) fromgraphGenFile}
+   * @throws Exception
+   */
+  @Test
+  public void testFromGraphGenFileWithEdgeCheck() throws Exception {
+    String graphGenFile =
+      EPGMDatabaseGraphGenTest.class.getResource("/data/graphgen/io_test.gg")
+        .getFile();
+
+    GraphTransactions<GraphHeadPojo, VertexPojo, EdgePojo> graphTransactions =
+      GraphTransactions.fromGraphGenFile(graphGenFile, getExecutionEnvironment());
+
+    GraphTransaction<GraphHeadPojo, VertexPojo, EdgePojo> graphTransaction =
+    graphTransactions.getTransactions().collect().get(1);
+
+    assertEquals("Wrong edge count", 2, graphTransaction.getEdges().size());
 
   }
 }
