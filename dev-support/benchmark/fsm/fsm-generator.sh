1d3b1022d74d02529c36b16ced59c7372f06457c
Kevin Gomez <k.gomez@freenet.de>
2016-07-05 13:08:51 +0200

    added documentation to fsm-generator.sh (#187)

--- dev-support/benchmark/fsm/fsm-generator.sh
+++ dev-support/benchmark/fsm/fsm-generator.sh
@@ -4,11 +4,16 @@
 # File to run the fsm generator multiple times
 ######################################################
 
+#Flink root directory
 FLINK="/usr/local/flink-1.0.0"
+#Used jar
 JAR="fsm.jar"
+#Used generator running class
 CLASS="org.gradoop.examples.datagen.PredictableTransactionsGeneratorRunner"
+#Output directory in hdfs
 OUTPUT="hdfs:///user/hduser/input/datagen/"
 
+#Running commands
 ${FLINK}/bin/flink run -p 32 -c ${CLASS} ${JAR} -o ${OUTPUT} -gc 100 -gs 1
 ${FLINK}/bin/flink run -p 32 -c ${CLASS} ${JAR} -o ${OUTPUT} -gc 1000 -gs 1
 ${FLINK}/bin/flink run -p 32 -c ${CLASS} ${JAR} -o ${OUTPUT} -gc 10000 -gs 1
