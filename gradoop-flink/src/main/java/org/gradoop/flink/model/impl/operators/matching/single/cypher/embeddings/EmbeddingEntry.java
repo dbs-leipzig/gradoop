fab0e6024f5dd2f3aae6e6c1f4d3853e223cabff
DarthMax <max@kopfueber.org>
2016-11-08 16:54:30 +0100

    Add API for physical operators and embeddings

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/embeddings/EmbeddingEntry.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings;
+
+import org.gradoop.common.model.impl.id.GradoopId;
+import org.gradoop.common.model.impl.properties.PropertyList;
+
+import java.util.Optional;
+
+/**
+ * Represents an entry in an embedding
+ */
+public interface EmbeddingEntry {
+  /**
+   * Returns the identifier of the element
+   * @return id
+   */
+  GradoopId getId();
+
+  /**
+   * Returns the list of properties of this element
+   * @return properties
+   */
+  Optional<PropertyList>  getProperties();
+}
---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/CartesianProduct.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.common.operators.base.CrossOperatorBase;
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+
+/**
+ * Creates the Cartesian Product of two embeddings.
+ */
+public class CartesianProduct implements PhysicalOperator {
+
+  /**
+   * Left hand side embeddings
+   */
+  private final DataSet<Embedding> lhs;
+  /**
+   * right hand side embeddings
+   */
+  private final DataSet<Embedding> rhs;
+  /**
+   * Cross Hint
+   */
+  private final CrossOperatorBase.CrossHint crossHint;
+
+  /**
+   * New Cartesian Product Operator
+   *
+   * @param lhs the left hand side embedding
+   * @param rhs the right hand side embedding
+   * @param crossHint cross hint
+   */
+  public CartesianProduct(DataSet<Embedding> lhs, DataSet<Embedding> rhs,
+    CrossOperatorBase.CrossHint crossHint) {
+    this.lhs = lhs;
+    this.rhs = rhs;
+    this.crossHint = crossHint;
+  }
+
+  /**
+   * New Cartesian Product Operator with default cross hint
+   * @param lhs left hand side embedding
+   * @param rhs right hand side embedding
+   */
+  public CartesianProduct(DataSet<Embedding> lhs, DataSet<Embedding> rhs) {
+    this(lhs, rhs, CrossOperatorBase.CrossHint.OPTIMIZER_CHOOSES);
+  }
+
+  @Override
+  public DataSet<Embedding> evaluate() {
+    return null;
+  }
+}
