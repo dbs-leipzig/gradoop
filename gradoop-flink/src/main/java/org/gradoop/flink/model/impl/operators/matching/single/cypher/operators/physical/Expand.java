178366560f5dd36ed3a7feee0ac7a8e528e5e414
Max <max@kopfueber.org>
2016-11-09 13:27:52 +0100

    [#381] Add API for physical operators and embeddings
    * Add API for physical operators and embeddings
    * Add variable mapping to FilterEmbedding and ValueJoin
    * Initial integration tests for physical operators
    * fixes #381

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/Expand.java
@@ -0,0 +1,104 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.common.operators.base.JoinOperatorBase;
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.utils.ExpandDirection;
+
+/**
+ * Expands an vertex along the edges. The number of hops can be specified via upper and lower bound
+ * The input embedding is appended by 2 Entries, the first one represents the path,
+ * the second one the end vertex
+ */
+public class Expand implements PhysicalOperator {
+
+  /**
+   * Input Embeddings
+   */
+  private final DataSet<Embedding> input;
+  /**
+   * Candidate edges
+   */
+  private final DataSet<Embedding> candidateEdges;
+  /**
+   * specifies the input column that will be expanded
+   */
+  private final int expandColumn;
+  /**
+   * minimum hops
+   */
+  private final int lowerBound;
+  /**
+   * maximum hops
+   */
+  private final int upperBound;
+  /**
+   * expand direction
+   */
+  private final ExpandDirection direction;
+  /**
+   * join hint
+   */
+  private final JoinOperatorBase.JoinHint joinHint;
+
+  /**
+   * New Expand One Operator
+   *
+   * @param input the embedding which should be expanded
+   * @param candidateEdges candidate edges along which we expand
+   * @param expandColumn specifies the colum that represents the vertex from which we expand
+   * @param lowerBound specifies the minimum hops we want to expand
+   * @param upperBound specifies the maximum hops we want to expand
+   * @param direction direction of the expansion {@see ExpandDirection}
+   * @param joinHint join strategy
+   */
+  public Expand(DataSet<Embedding> input, DataSet<Embedding> candidateEdges, int expandColumn,
+    int lowerBound, int upperBound, ExpandDirection direction, JoinOperatorBase.JoinHint joinHint) {
+    this.input = input;
+    this.candidateEdges = candidateEdges;
+    this.expandColumn = expandColumn;
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+    this.direction = direction;
+    this.joinHint = joinHint;
+  }
+
+  /**
+   * New Expand One Operator with default join hint
+   *
+   * @param input the embedding which should be expanded
+   * @param candidateEdges candidate edges along which we expand
+   * @param expandColumn specifies the colum that represents the vertex from which we expand
+   * @param lowerBound specifies the minimum hops we want to expand
+   * @param upperBound specifies the maximum hops we want to expand
+   * @param direction direction of the expansion {@see ExpandDirection}
+   */
+  public Expand(DataSet<Embedding> input, DataSet<Embedding> candidateEdges, int expandColumn,
+    int lowerBound, int upperBound, ExpandDirection direction) {
+
+    this(input, candidateEdges, expandColumn, lowerBound, upperBound, direction,
+      JoinOperatorBase.JoinHint .BROADCAST_HASH_FIRST);
+  }
+
+  @Override
+  public DataSet<Embedding> evaluate() {
+    return null;
+  }
+}
---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/ProjectEmbeddings.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Projects an Embedding by a set of properties.
+ * For each entry in the embedding a different property set can be specified
+ */
+public class ProjectEmbeddings implements PhysicalOperator {
+
+  /**
+   * Input Embeddings
+   */
+  private final DataSet<Embedding> input;
+  /**
+   * Names of the properties that will be kept in the projection
+   */
+  private final HashMap<Integer, List<String>> propertyKeys;
+
+  /**
+   * Creates a new embedding projection operator
+   * @param input Embeddings that should be projected
+   * @param propertyKeys HashMap of property labels, keys are the columns of the entry,
+   *                     values are property keys
+   */
+  public ProjectEmbeddings(DataSet<Embedding> input, HashMap<Integer, List<String>> propertyKeys) {
+    this.input = input;
+    this.propertyKeys = propertyKeys;
+  }
+
+  @Override
+  public DataSet<Embedding> evaluate() {
+    return null;
+  }
+}
