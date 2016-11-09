ba23c4af209f29c0123441bd89f360b5effecd2c
Max <max@kopfueber.org>
2016-11-09 13:27:52 +0100

    [#381] Add API for physical operators and embeddings
    * Add API for physical operators and embeddings
    * Add variable mapping to FilterEmbedding and ValueJoin
    * Initial integration tests for physical operators
    * fixes #381

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/FilterEmbeddings.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+import org.s1ck.gdl.model.cnf.CNF;
+
+import java.util.HashMap;
+
+/**
+ * Filters a set of Embeddings by the given predicates
+ * The resulting embeddings have the same schema as the input embeddings
+ */
+public class FilterEmbeddings implements PhysicalOperator {
+  /**
+   * Candidate Embeddings
+   */
+  private final DataSet<Embedding> input;
+  /**
+   * Predicates in conjunctive normal form
+   */
+  private CNF predicates;
+  /**
+   * Maps variable names to embedding entries;
+   */
+  private final HashMap<String,Integer> variableMapping;
+
+
+  /**
+   * New embedding filter operator
+   * @param input Candidate embeddings
+   * @param predicates Predicates to used for filtering
+   * @param variableMapping Maps variable names to embedding entries
+   */
+  public FilterEmbeddings(DataSet<Embedding> input, CNF predicates,
+    HashMap<String, Integer> variableMapping) {
+    this.input = input;
+    this.predicates = predicates;
+    this.variableMapping = variableMapping;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public DataSet<Embedding> evaluate() {
+
+    return null;
+  }
+}
---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/FilterVertices.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.common.model.impl.pojo.Vertex;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.IdEntry;
+import org.s1ck.gdl.model.cnf.CNF;
+
+/**
+ * Filters a set of Vertices by the given predicates
+ * Returns an Embedding with one
+ * {@link IdEntry} entry
+ */
+public class FilterVertices implements PhysicalOperator {
+  /**
+   * Input vertices
+   */
+  private final DataSet<Vertex> input;
+  /**
+   * Predicates in conjunctive normal form
+   */
+  private final CNF predicates;
+
+  /**
+   * New vertex filter operator
+   *
+   * @param input candidate vertices
+   * @param predicates predicates used for filtering
+   */
+  public FilterVertices(DataSet<Vertex> input, CNF predicates) {
+    this.input = input;
+    this.predicates = predicates;
+  }
+
+  @Override
+  public DataSet<Embedding> evaluate() {
+
+    return null;
+  }
+}
