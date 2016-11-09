6ff2454fd13ce2f9a2015a15ab0e6a7789da560e
Max <max@kopfueber.org>
2016-11-09 13:27:52 +0100

    [#381] Add API for physical operators and embeddings
    * Add API for physical operators and embeddings
    * Add variable mapping to FilterEmbedding and ValueJoin
    * Initial integration tests for physical operators
    * fixes #381

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/FilterAndProjectEmbeddings.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+import org.s1ck.gdl.model.cnf.CNF;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Filters a List of Embeddings by predicates and projects the remaining to the specified properties
+ * The resulting embeddings have the same schema as the input embeddings
+ */
+public class FilterAndProjectEmbeddings implements PhysicalOperator {
+
+  /**
+   * CandidateEmbeddings
+   */
+  private final DataSet<Embedding> input;
+  /**
+   * Predicates used for filtering in Conjunctive Normal Form
+   */
+  private final CNF predicates;
+  /**
+   * Property names that will be kept in projection. Hash keys defines column index
+   */
+  private final HashMap<Integer, List<String>> propertyKeys;
+
+  /**
+   * New Operator
+   *
+   * @param input Candidate Embeddings
+   * @param predicates Predicates that will be used to filter candidates
+   * @param propertyKeys HashMap of property labels, keys are the columns of the entry,
+   *                     values are property keys
+   */
+  public FilterAndProjectEmbeddings(DataSet<Embedding> input, CNF predicates,
+    HashMap<Integer, List<String>> propertyKeys) {
+    this.input = input;
+    this.predicates = predicates;
+    this.propertyKeys = propertyKeys;
+  }
+
+  @Override
+  public DataSet<Embedding> evaluate() {
+    return null;
+  }
+}
---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/ValueJoin.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.operators.physical;
+
+import org.apache.flink.api.java.DataSet;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+import org.s1ck.gdl.model.cnf.CNF;
+
+import java.util.HashMap;
+
+/**
+ * This operator joins two possibly disjunct data sets by predicates only concerning properties
+ * e.g.
+ * <pre>
+ *   MATCH (a:Department), (b)-[:X]->(c:Person {name: "Max") WHERE a.prop = b.prop
+ * </pre>
+ */
+public class ValueJoin implements PhysicalOperator {
+
+  /**
+   * Embeddings of the left side
+   */
+  private final DataSet<Embedding> lhs;
+  /**
+   * Embeddings of the right side
+   */
+  private final DataSet<Embedding> rhs;
+  /**
+   * predicates used for the join
+   */
+  private final CNF joinCriteria;
+  /**
+   * Maps variable names to embedding entries;
+   */
+  private final HashMap<String,Integer> variableMapping;
+
+  /**
+   * New value join operator
+   *
+   * @param lhs left hand side data set
+   * @param rhs right hand side data set
+   * @param joinCriteria join criteria
+   * @param variableMapping Maps variable names to embedding entries
+   */
+  public ValueJoin(DataSet<Embedding> lhs, DataSet<Embedding> rhs, CNF joinCriteria,
+    HashMap<String, Integer> variableMapping) {
+    this.lhs = lhs;
+    this.rhs = rhs;
+    this.joinCriteria = joinCriteria;
+    this.variableMapping = variableMapping;
+  }
+
+  @Override
+  public DataSet<Embedding> evaluate() {
+    return null;
+  }
+}
