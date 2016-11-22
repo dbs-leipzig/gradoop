87666fadfa22745ee8646c93891d1c3e138f9ef1
Stephan Kemper <stephan.ke@gmx.de>
2016-11-22 16:54:24 +0100

    Changed to dataset of vertices and removed duplicates in dictionary creation.

--- gradoop-flink/src/main/java/org/gradoop/flink/io/impl/tlf/functions/EdgeLabelList.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/tlf/functions/EdgeLabelList.java
@@ -17,28 +17,22 @@
 
 package org.gradoop.flink.io.impl.tlf.functions;
 
-import com.google.common.collect.Lists;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.util.Collector;
 import org.gradoop.common.model.impl.pojo.Edge;
 import org.gradoop.flink.representation.tuples.GraphTransaction;
 
-import java.util.List;
-
 /**
- * Creates a list containing all edge labels.
+ * Collects all edge labels.
  */
 public class EdgeLabelList implements
-  FlatMapFunction<GraphTransaction, List<String>> {
+  FlatMapFunction<GraphTransaction, String> {
 
   @Override
-  public void flatMap(
-    GraphTransaction graphTransaction, Collector<List<String>>
-    collector) throws Exception {
-    List<String> list = Lists.newLinkedList();
+  public void flatMap(GraphTransaction graphTransaction, Collector<String> collector)
+    throws Exception {
     for (Edge edge : graphTransaction.getEdges()) {
-      list.add(edge.getLabel());
+      collector.collect(edge.getLabel());
     }
-    collector.collect(list);
   }
 }
--- gradoop-flink/src/main/java/org/gradoop/flink/io/impl/tlf/functions/TLFDictionaryMapGroupReducer.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/tlf/functions/TLFDictionaryMapGroupReducer.java
@@ -22,29 +22,23 @@ import com.google.common.collect.Maps;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.util.Collector;
 
-import java.util.List;
 import java.util.Map;
 
 /**
  * Reduces the incoming list of vertex or edge labels to a single HashMap
  * containing those labels without duplicates and an ascending integer value.
  */
-public class TLFDictionaryMapGroupReducer implements
-  GroupReduceFunction<List<String>, Map<String,
-  Integer>> {
+public class TLFDictionaryMapGroupReducer
+  implements GroupReduceFunction<String, Map<String, Integer>> {
 
   @Override
-  public void reduce(Iterable<List<String>> iterable,
-    Collector<Map<String, Integer>> collector) throws Exception {
+  public void reduce(Iterable<String> iterable, Collector<Map<String, Integer>> collector)
+    throws Exception {
     Map<String, Integer> newMap = Maps.newHashMap();
     int id = 0;
-    for (List<String> list : iterable) {
-      for (int i = 0; i < list.size(); i++) {
-        if (!newMap.containsKey(list.get(i))) {
-          newMap.put(list.get(i), id);
-          id++;
-        }
-      }
+    for (String label : iterable) {
+      newMap.put(label, id);
+      id++;
     }
     collector.collect(newMap);
   }
