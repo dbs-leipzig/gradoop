e9d73f3059e82767fd997e59b0edb721a93def9d
Max <max@kopfueber.org>
2016-11-29 16:41:48 +0100

    [#413] Add Expand operator
    * fixes #413

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/functions/PostProcessExpandResult.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gradoop.flink.model.impl.operators.matching.single.cypher.functions;
+
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.util.Collector;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
+import org.gradoop.flink.model.impl.operators.matching.single.cypher.utils.ExpandIntermediateResult;
+
+/**
+ * Postprocesses the expand iteration results
+ * 1. Remove paths below lower bound length
+ * 2. Remove results that do not match circle condition
+ * 3. Turn intermediate results into embeddings
+ */
+public class PostProcessExpandResult
+  extends RichFlatMapFunction<ExpandIntermediateResult, Embedding> {
+
+  /**
+   * Holds the minimum path size calculated from lower bound
+   */
+  private final int minPathLength;
+  /**
+   * Specifies the base column which should be equal to the paths end column
+   */
+  private final int closingColumn;
+
+  /**
+   * Create a new Postprocess function
+   *
+   * @param lowerBound the lower bound path length
+   * @param closingColumn the base column which should equal the paths end column
+   */
+  public PostProcessExpandResult(int lowerBound, int closingColumn) {
+    this.minPathLength = lowerBound * 2 - 1;
+    this.closingColumn = closingColumn;
+  }
+
+  @Override
+  public void flatMap(ExpandIntermediateResult value, Collector<Embedding> out) throws Exception {
+    if (value.pathSize() < minPathLength) {
+      return;
+    }
+
+    if (closingColumn >= 0 && !value.getBase().getEntry(closingColumn).contains(value.getEnd())) {
+      return;
+    }
+
+    out.collect(value.toEmbedding());
+  }
+}
--- gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/OperatorIntegrationTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/matching/single/cypher/operators/physical/OperatorIntegrationTest.java
@@ -29,236 +29,237 @@ import org.gradoop.flink.model.impl.operators.matching.common.query.predicates.C
 import org.gradoop.flink.model.impl.operators.matching.single.cypher.embeddings.Embedding;
 import org.gradoop.flink.util.FlinkAsciiGraphLoader;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.gradoop.flink.model.impl.operators.matching.single.cypher.utils.ExpandDirection.*;
 
+@Ignore
 public class OperatorIntegrationTest extends GradoopFlinkTestBase {
 
-//  private FlinkAsciiGraphLoader testGraphs;
-//
-//  @Before
-//  public void loadGraph() throws Exception {
-//    this.testGraphs = getLoaderFromFile(
-//      OperatorIntegrationTest.class.getResource("/data/gdl/physicalOperators.gdl").getFile()
-//    );
-//  }
-//
-//  /**
-//   * MATCH (p1:Person {name: "Alice"})-[r1:worked_at]->(o)
-//   * WHERE r1.active=1
-//   * RETURN *
-//   */
-//  @Test
-//  public void simplePredicateQueryTest() throws Exception {
-//    QueryHandler query = new QueryHandler(
-//      "MATCH (p1:Person {name: \"Alice\"})-[r1:worked_at]->(o) " +
-//      "WHERE r1.active=1"
-//    );
-//
-//    LogicalGraph graph = testGraphs.getLogicalGraphByVariable("g");
-//
-//    CNF predicates = query.getPredicates();
-//    CNF p1Predicate = predicates.getSubCNF(Sets.newHashSet("p1"));
-//    CNF r1Predicate = predicates.getSubCNF(Sets.newHashSet("r1"));
-//
-//    DataSet<Embedding> p1 =
-//      new FilterVertices(graph.getVertices(), p1Predicate).evaluate();
-//    DataSet<Embedding> r1 =
-//      new FilterEdges(graph.getEdges(), r1Predicate).evaluate();
-//
-//    System.out.println("p1.count() = " + p1.count());
-//    System.out.println("r1.count() = " + r1.count());
-//
-//    //DataSet<Embedding> res = new ExpandOne(p1,r1,0, ExpandDirection.OUT).evaluate();
-//
-//    //System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//  /**
-//   * MATCH (a:Person)-[]->(b:Person)
-//   * WHERE a.age > b.age OR a.name=b.name
-//   * RETURN *
-//   */
-//  @Test
-//  public void CrossPredicateTest() throws Exception {
-//    LogicalGraph graph = loadGraph("","g");
-//
-//    QueryHandler query = new QueryHandler(
-//      "MATCH (a:Person)-[]->(b:Person)" +
-//      "WHERE a.age > b.age OR a.name=b.name"
-//    );
-//
-//    CNF predicates = query.getPredicates();
-//
-//    DataSet<Embedding> vertices =
-//      new FilterAndProjectVertices(
-//          graph.getVertices(),
-//          predicates.getSubCNF(Sets.newHashSet("a")),
-//          Lists.newArrayList("age","name")
-//      ).evaluate();
-//
-//    DataSet<Embedding> edges = new ProjectEdges(graph.getEdges()).evaluate();
-//
-//    DataSet<Embedding> aExpanded = new ExpandOne(vertices,edges,0, OUT).evaluate();
-//
-//    DataSet<Embedding> ab = new JoinEmbeddings(aExpanded,vertices,2,0).evaluate();
-//
-//    HashMap<String, Integer> mapping = new HashMap<>();
-//    mapping.put("a",0);
-//    mapping.put("b",2);
-//    DataSet<Embedding> res = new FilterEmbeddings(
-//      ab,
-//      predicates.getSubCNF(Sets.newHashSet("a","b")),
-//      mapping
-//    ).evaluate();
-//
-//    System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//  /**
-//   * MATCH (n)-[]->(m)-[]->(o)
-//   * RETURN *
-//   */
-//  @Test
-//  public void homomorphismTest() throws Exception {
-//    LogicalGraph graph = loadGraph("","g");
-//
-//    DataSet<Embedding> vertices =
-//      new ProjectVertices(graph.getVertices()).evaluate();
-//    DataSet<Embedding> edges =
-//      new ProjectEdges(graph.getEdges()).evaluate();
-//
-//    DataSet<Embedding> a = new ExpandOne(
-//      vertices, edges, 0,
-//      OUT, MatchStrategy.HOMOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
-//    ).evaluate();
-//
-//    DataSet<Embedding> res = new ExpandOne(a, edges, 2,
-//      OUT, MatchStrategy.HOMOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
-//    ).evaluate();
-//
-//    System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//  /**
-//   * MATCH (n)-[]->(m)-[]->(o)
-//   * RETURN *
-//   */
-//  @Test
-//  public void isomorphismTest() throws Exception {
-//    LogicalGraph graph = loadGraph("","g");
-//
-//    DataSet<Embedding> vertices =
-//      new ProjectVertices(graph.getVertices()).evaluate();
-//    DataSet<Embedding> edges =
-//      new ProjectEdges(graph.getEdges()).evaluate();
-//
-//    DataSet<Embedding> a = new ExpandOne(
-//      vertices, edges, 0,
-//      OUT, MatchStrategy.ISOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
-//    ).evaluate();
-//
-//    DataSet<Embedding> res = new ExpandOne(a, edges, 2,
-//      OUT, MatchStrategy.ISOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
-//    ).evaluate();
-//
-//    System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//  /**
-//   * MATCH (n)-[*2..3]->(m)
-//   * RETURN *
-//   */
-//  @Test
-//  public void variableLengthPathQueryTest() throws Exception{
-//    LogicalGraph graph = loadGraph("","g");
-//
-//    DataSet<Embedding> n = new ProjectVertices(graph.getVertices()).evaluate();
-//    DataSet<Embedding> edges = new ProjectEdges(graph.getEdges()).evaluate();
-//
-//    DataSet<Embedding> res = new Expand(n,edges,0,2,3,OUT).evaluate();
-//
-//    System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//  /**
-//   * MATCH (a:Department), (b:City)
-//   * RETURN *
-//   */
-//  @Test
-//  public void cartesianProductTest() throws Exception {
-//    LogicalGraph graph = loadGraph("","g");
-//    QueryHandler query = new QueryHandler("MATCH (a:Department), (b:City)");
-//    CNF predicates = query.getPredicates();
-//
-//    DataSet<Embedding> a = new FilterVertices(
-//      graph.getVertices(),
-//      predicates.getSubCNF(Sets.newHashSet("a"))
-//    ).evaluate();
-//    DataSet<Embedding> b = new FilterVertices(
-//      graph.getVertices(),
-//      predicates.getSubCNF(Sets.newHashSet("a"))
-//    ).evaluate();
-//
-//    DataSet<Embedding> res = new CartesianProduct(a,b).evaluate();
-//
-//    System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//  /**
-//   * ⁠MATCH (a:Department), (b)-[]->(c:Person {name: "Alice")
-//   * WHERE a.prop = b.prop
-//   * RETURN *
-//   */
-//  @Test
-//  public void valueJoinTest() throws Exception {
-//    LogicalGraph graph = loadGraph("","g");
-//    QueryHandler query = new QueryHandler(
-//      " MATCH (a:Department),(b)-[]->(c:Person {name: \"Alice\")" +
-//      " WHERE a.prop = b.prop"
-//    );
-//    CNF predicates = query.getPredicates();
-//
-//    DataSet<Embedding> a =
-//      new FilterAndProjectVertices(
-//        graph.getVertices(),
-//        predicates.getSubCNF(Sets.newHashSet("a")),
-//        Lists.newArrayList("prop")
-//      ).evaluate();
-//
-//    DataSet<Embedding> c = new FilterVertices(
-//      graph.getVertices(),
-//      predicates.getSubCNF(Sets.newHashSet("c"))
-//    ).evaluate();
-//
-//    DataSet<Embedding> edges = new ProjectEdges(graph.getEdges()).evaluate();
-//
-//    DataSet<Embedding> cexpand = new ExpandOne(c,edges,0,IN).evaluate();
-//
-//    DataSet<Embedding> b =
-//      new ProjectVertices(graph.getVertices(), Lists.newArrayList("prop")).evaluate();
-//
-//    DataSet<Embedding> bc = new JoinEmbeddings(cexpand,b,2,0).evaluate();
-//
-//    HashMap<String, Integer> mapping = new HashMap<>();
-//    mapping.put("a",0);
-//    mapping.put("b",3);
-//
-//    DataSet<Embedding> res = new ValueJoin(
-//      a,
-//      bc,
-//      predicates.getSubCNF(Sets.newHashSet("a","b")),
-//      mapping
-//    ).evaluate();
-//
-//    System.out.println("res.collect() = " + res.collect());
-//  }
-//
-//   private LogicalGraph loadGraph(String dataGraph, String variable) {
-//    FlinkAsciiGraphLoader loader = getLoaderFromString(dataGraph);
-//    return loader.getLogicalGraphByVariable(variable);
-//  }
+  private FlinkAsciiGraphLoader testGraphs;
+
+  @Before
+  public void loadGraph() throws Exception {
+    this.testGraphs = getLoaderFromFile(
+      OperatorIntegrationTest.class.getResource("/data/gdl/physicalOperators.gdl").getFile()
+    );
+  }
+
+  /**
+   * MATCH (p1:Person {name: "Alice"})-[r1:worked_at]->(o)
+   * WHERE r1.active=1
+   * RETURN *
+   */
+  @Test
+  public void simplePredicateQueryTest() throws Exception {
+    QueryHandler query = new QueryHandler(
+      "MATCH (p1:Person {name: \"Alice\"})-[r1:worked_at]->(o) " +
+      "WHERE r1.active=1"
+    );
+
+    LogicalGraph graph = testGraphs.getLogicalGraphByVariable("g");
+
+    CNF predicates = query.getPredicates();
+    CNF p1Predicate = predicates.getSubCNF(Sets.newHashSet("p1"));
+    CNF r1Predicate = predicates.getSubCNF(Sets.newHashSet("r1"));
+
+    DataSet<Embedding> p1 =
+      new FilterVertices(graph.getVertices(), p1Predicate).evaluate();
+    DataSet<Embedding> r1 =
+      new FilterEdges(graph.getEdges(), r1Predicate).evaluate();
+
+    //DataSet<Embedding> res = new  JoinEmbeddings(p1,r1,0, ExpandDirection.OUT).evaluate();
+
+    //System.out.println("res.collect() = " + res.collect());
+  }
+
+  /**
+   * MATCH (a:Person)-[]->(b:Person)
+   * WHERE a.age > b.age OR a.name=b.name
+   * RETURN *
+   */
+  @Test
+  public void CrossPredicateTest() throws Exception {
+    LogicalGraph graph = loadGraph("","g");
+
+    QueryHandler query = new QueryHandler(
+      "MATCH (a:Person)-[]->(b:Person)" +
+      "WHERE a.age > b.age OR a.name=b.name"
+    );
+
+    CNF predicates = query.getPredicates();
+
+    DataSet<Embedding> vertices =
+      new FilterAndProjectVertices(
+          graph.getVertices(),
+          predicates.getSubCNF(Sets.newHashSet("a")),
+          Lists.newArrayList("age","name")
+      ).evaluate();
+
+    DataSet<Embedding> edges = new ProjectEdges(graph.getEdges()).evaluate();
+
+    DataSet<Embedding> aExpanded = new ExpandOne(vertices,edges,0, OUT).evaluate();
+
+    DataSet<Embedding> ab = new JoinEmbeddings(aExpanded,vertices,2,0).evaluate();
+
+    HashMap<String, Integer> mapping = new HashMap<>();
+    mapping.put("a",0);
+    mapping.put("b",2);
+    DataSet<Embedding> res = new FilterEmbeddings(
+      ab,
+      predicates.getSubCNF(Sets.newHashSet("a","b")),
+      mapping
+    ).evaluate();
+
+    System.out.println("res.collect() = " + res.collect());
+  }
+
+  /**
+   * MATCH (n)-[]->(m)-[]->(o)
+   * RETURN *
+   */
+  @Test
+  public void homomorphismTest() throws Exception {
+    LogicalGraph graph = loadGraph("","g");
+
+    DataSet<Embedding> vertices =
+      new ProjectVertices(graph.getVertices()).evaluate();
+    DataSet<Embedding> edges =
+      new ProjectEdges(graph.getEdges()).evaluate();
+
+    DataSet<Embedding> a = new ExpandOne(
+      vertices, edges, 0,
+      OUT, MatchStrategy.HOMOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
+    ).evaluate();
+
+    DataSet<Embedding> res = new ExpandOne(a, edges, 2,
+      OUT, MatchStrategy.HOMOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
+    ).evaluate();
+
+    System.out.println("res.collect() = " + res.collect());
+  }
+
+  /**
+   * MATCH (n)-[]->(m)-[]->(o)
+   * RETURN *
+   */
+  @Test
+  public void isomorphismTest() throws Exception {
+    LogicalGraph graph = loadGraph("","g");
+
+    DataSet<Embedding> vertices =
+      new ProjectVertices(graph.getVertices()).evaluate();
+    DataSet<Embedding> edges =
+      new ProjectEdges(graph.getEdges()).evaluate();
+
+    DataSet<Embedding> a = new ExpandOne(
+      vertices, edges, 0,
+      OUT, MatchStrategy.ISOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
+    ).evaluate();
+
+    DataSet<Embedding> res = new ExpandOne(a, edges, 2,
+      OUT, MatchStrategy.ISOMORPHISM, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES
+    ).evaluate();
+
+    System.out.println("res.collect() = " + res.collect());
+  }
+
+  /**
+   * MATCH (n)-[*2..3]->(m)
+   * RETURN *
+   */
+  @Test
+  public void variableLengthPathQueryTest() throws Exception{
+    LogicalGraph graph = loadGraph("","g");
+
+    DataSet<Embedding> n = new ProjectVertices(graph.getVertices()).evaluate();
+    DataSet<Embedding> edges = new ProjectEdges(graph.getEdges()).evaluate();
+
+    DataSet<Embedding> res =
+      new Expand(n,edges,0,2,3,OUT, new ArrayList<>(), new ArrayList<>(),-1).evaluate();
+
+    System.out.println("res.collect() = " + res.collect());
+  }
+
+  /**
+   * MATCH (a:Department), (b:City)
+   * RETURN *
+   */
+  @Test
+  public void cartesianProductTest() throws Exception {
+    LogicalGraph graph = loadGraph("","g");
+    QueryHandler query = new QueryHandler("MATCH (a:Department), (b:City)");
+    CNF predicates = query.getPredicates();
+
+    DataSet<Embedding> a = new FilterVertices(
+      graph.getVertices(),
+      predicates.getSubCNF(Sets.newHashSet("a"))
+    ).evaluate();
+    DataSet<Embedding> b = new FilterVertices(
+      graph.getVertices(),
+      predicates.getSubCNF(Sets.newHashSet("a"))
+    ).evaluate();
+
+    DataSet<Embedding> res = new CartesianProduct(a,b).evaluate();
+
+    System.out.println("res.collect() = " + res.collect());
+  }
+
+  /**
+   * ⁠MATCH (a:Department), (b)-[]->(c:Person {name: "Alice")
+   * WHERE a.prop = b.prop
+   * RETURN *
+   */
+  @Test
+  public void valueJoinTest() throws Exception {
+    LogicalGraph graph = loadGraph("","g");
+    QueryHandler query = new QueryHandler(
+      " MATCH (a:Department),(b)-[]->(c:Person {name: \"Alice\")" +
+      " WHERE a.prop = b.prop"
+    );
+    CNF predicates = query.getPredicates();
+
+    DataSet<Embedding> a =
+      new FilterAndProjectVertices(
+        graph.getVertices(),
+        predicates.getSubCNF(Sets.newHashSet("a")),
+        Lists.newArrayList("prop")
+      ).evaluate();
+
+    DataSet<Embedding> c = new FilterVertices(
+      graph.getVertices(),
+      predicates.getSubCNF(Sets.newHashSet("c"))
+    ).evaluate();
+
+    DataSet<Embedding> edges = new ProjectEdges(graph.getEdges()).evaluate();
+
+    DataSet<Embedding> cexpand = new ExpandOne(c,edges,0,IN).evaluate();
+
+    DataSet<Embedding> b =
+      new ProjectVertices(graph.getVertices(), Lists.newArrayList("prop")).evaluate();
+
+    DataSet<Embedding> bc = new JoinEmbeddings(cexpand,b,2,0).evaluate();
+
+    HashMap<String, Integer> mapping = new HashMap<>();
+    mapping.put("a",0);
+    mapping.put("b",3);
+
+    DataSet<Embedding> res = new ValueJoin(
+      a,
+      bc,
+      predicates.getSubCNF(Sets.newHashSet("a","b")),
+      mapping
+    ).evaluate();
+
+    System.out.println("res.collect() = " + res.collect());
+  }
+
+   private LogicalGraph loadGraph(String dataGraph, String variable) {
+    FlinkAsciiGraphLoader loader = getLoaderFromString(dataGraph);
+    return loader.getLogicalGraphByVariable(variable);
+  }
 }
