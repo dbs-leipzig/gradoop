83045a509e327b056775441604a8e669531cc348
DarthMax <max@kopfueber.org>
2017-01-20 18:54:26 +0100

    Add variable binding for single vertex traversal matching

--- gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/common/functions/AddGraphElementToNewGraph.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/matching/common/functions/AddGraphElementToNewGraph.java
@@ -23,6 +23,10 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.gradoop.common.model.impl.pojo.GraphElement;
 import org.gradoop.common.model.impl.pojo.GraphHead;
 import org.gradoop.common.model.impl.pojo.GraphHeadFactory;
+import org.gradoop.common.model.impl.properties.PropertyValue;
+import org.gradoop.flink.model.impl.operators.matching.single.PatternMatching;
+
+import java.util.HashMap;
 
 /**
  * (GE) -> (GE (+ GraphHead), GraphHead)
@@ -41,24 +45,44 @@ public class AddGraphElementToNewGraph<GE extends GraphElement>
    */
   private final GraphHeadFactory graphHeadFactory;
   /**
+   * Variable assigned to the query vertex
+   */
+  private final String variable;
+  /**
    * Reduce instantiations
    */
   private final Tuple2<GE, GraphHead> reuseTuple;
+  /**
+   * Reuse map for variable mapping
+   */
+  private final HashMap<PropertyValue, PropertyValue> reuseVariableMapping;
 
   /**
    * Constructor
    *
    * @param graphHeadFactory EPGM graph head factory
+   * @param variable Variable assigned to the only query vertex
    */
-  public AddGraphElementToNewGraph(GraphHeadFactory graphHeadFactory) {
+  public AddGraphElementToNewGraph(GraphHeadFactory graphHeadFactory, String variable) {
     this.graphHeadFactory = graphHeadFactory;
+    this.variable = variable;
     reuseTuple = new Tuple2<>();
+    reuseVariableMapping = new HashMap<>();
   }
 
   @Override
   public Tuple2<GE, GraphHead> map(GE value) throws Exception {
+    reuseVariableMapping.clear();
+    reuseVariableMapping.put(
+      PropertyValue.create(this.variable),
+      PropertyValue.create(value.getId())
+    );
+
     GraphHead graphHead = graphHeadFactory.createGraphHead();
+    graphHead.setProperty(PatternMatching.VARIABLE_MAPPING_KEY, reuseVariableMapping);
+
     value.addGraphId(graphHead.getId());
+
     reuseTuple.f0 = value;
     reuseTuple.f1 = graphHead;
     return reuseTuple;
--- gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/matching/common/functions/AddGraphElementToNewGraphTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/matching/common/functions/AddGraphElementToNewGraphTest.java
@@ -1,3 +1,16 @@
+package org.gradoop.flink.model.impl.operators.matching.common.functions;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.gradoop.common.model.impl.pojo.GraphHead;
+import org.gradoop.common.model.impl.pojo.GraphHeadFactory;
+import org.gradoop.common.model.impl.pojo.Vertex;
+import org.gradoop.common.model.impl.pojo.VertexFactory;
+import org.gradoop.common.model.impl.properties.PropertyValue;
+import org.gradoop.flink.model.impl.operators.matching.single.PatternMatching;
+import org.junit.Test;
+
+import java.util.Map;
+
 import static org.junit.Assert.*;
 /*
  * This file is part of Gradoop.
@@ -17,5 +30,18 @@ import static org.junit.Assert.*;
  */
 
 public class AddGraphElementToNewGraphTest {
+  @Test
+  public void testVariableMappingCreation() throws Exception {
+    AddGraphElementToNewGraph<Vertex> udf =
+      new AddGraphElementToNewGraph<>(new GraphHeadFactory(), "a");
 
+    Vertex vertex = new VertexFactory().createVertex();
+    Tuple2<Vertex, GraphHead> result = udf.map(vertex);
+
+    assertTrue(result.f1.hasProperty(PatternMatching.VARIABLE_MAPPING_KEY));
+    Map<PropertyValue, PropertyValue> variableMapping =
+      result.f1.getPropertyValue(PatternMatching.VARIABLE_MAPPING_KEY).getMap();
+
+    assertEquals(vertex.getId(), variableMapping.get(PropertyValue.create("a")).getGradoopId());
+  }
 }
