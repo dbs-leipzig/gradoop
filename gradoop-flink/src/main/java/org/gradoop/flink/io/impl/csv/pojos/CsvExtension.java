55794d2ab2076e3dd7beaf332424d244e2db3367
venom590 <stephan.ke@gmx.de>
2017-01-24 22:48:23 +0100

    Added comments.

--- gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/pojos/CsvExtension.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/pojos/CsvExtension.java
@@ -1,11 +1,35 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.gradoop.flink.io.impl.csv.pojos;
 
 /**
- * Created by stephan on 04.11.16.
+ * This class is not part of the auto generated jaxb pojos so if needed modify with care.
+ * The extension to the generated class is needed because the datasource name and the domain name
+ * have to be saved together with the csv meta information.
  */
 public class CsvExtension extends Csv {
+    /**
+     * Name of the datasource where the, in csv described, object is located in.
+     */
     private String datasourceName;
-
+    /**
+     * Name of the domain where the, in csv described, object is located in.
+     */
     private String domainName;
 
     public String getDatasourceName() {
