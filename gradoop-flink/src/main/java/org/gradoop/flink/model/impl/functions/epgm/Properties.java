0571f417835079c8e81c01e18b044230d710d4df
Giacomo Bergami <giacomo@openmailbox.org>
2017-01-25 19:08:20 +0100

    on #511, after checking the class hierarchy, I generalized it for any EPGMElement

--- gradoop-flink/src/main/java/org/gradoop/flink/model/impl/functions/epgm/Properties.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/functions/epgm/Properties.java
@@ -20,26 +20,23 @@ package org.gradoop.flink.model.impl.functions.epgm;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.FunctionAnnotation;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.gradoop.common.model.api.entities.EPGMGraphHead;
-import org.gradoop.common.model.api.entities.EPGMLabeled;
-import org.gradoop.common.model.impl.properties.Properties;
-
+import org.gradoop.common.model.api.entities.EPGMElement;
 /**
  * EPGMGraphHead with properties => properties
  *
  * @param <L> EPGMGraphHead type having properties
  */
 @FunctionAnnotation.ForwardedFields("label->*")
-public class GraphHeadProperties<L extends EPGMGraphHead>
-  implements MapFunction<L, Properties>, KeySelector<L, Properties> {
+public class Properties<L extends EPGMElement>
+  implements MapFunction<L, org.gradoop.common.model.impl.properties.Properties>, KeySelector<L, org.gradoop.common.model.impl.properties.Properties> {
 
   @Override
-  public Properties map(L l) throws Exception {
+  public org.gradoop.common.model.impl.properties.Properties map(L l) throws Exception {
     return l.getProperties();
   }
 
   @Override
-  public Properties getKey(L l) throws Exception {
+  public org.gradoop.common.model.impl.properties.Properties getKey(L l) throws Exception {
     return l.getProperties();
   }
 }
