3f8268396480d54d7432aa5a581fd4d159e78d0a
Giacomo Bergami <giacomo@openmailbox.org>
2017-02-21 14:32:29 +0100

    #536, Interface implemented

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/api/operators/GraphGraphGraphCollectionToGraph.java
@@ -0,0 +1,13 @@
+package org.gradoop.flink.model.api.operators;
+
+import org.gradoop.flink.model.impl.LogicalGraph;
+
+/**
+ * Creates a {@link LogicalGraph} based on two input graphs and a {@link org.gradoop.flink.model.impl.GraphCollection}
+ *
+ * @see ReduceVertexFusion
+ *
+ * Created by Giacomo Bergami on 21/02/17.
+ */
+public interface GraphGraphGraphCollectionToGraph {
+}
