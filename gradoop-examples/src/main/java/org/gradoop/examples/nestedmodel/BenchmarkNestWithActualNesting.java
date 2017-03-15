3bd46aa74522c6e1c47b79e7d66afc8818cddf2e
Giacomo Bergami <giacomo@openmailbox.org>
2017-03-15 17:05:05 +0100

    Serialization problems. Removed my removed generalization (????)

--- gradoop-examples/src/main/java/org/gradoop/examples/nestedmodel/BenchmarkNestWithActualNesting.java
+++ gradoop-examples/src/main/java/org/gradoop/examples/nestedmodel/BenchmarkNestWithActualNesting.java
@@ -26,7 +26,6 @@ import org.gradoop.examples.grouping.GroupingRunner;
 import org.gradoop.examples.nestedmodel.benchmarks.BenchmarkResult;
 import org.gradoop.examples.nestedmodel.benchmarks.Time;
 import org.gradoop.flink.io.impl.graph.GraphDataSource;
-import org.gradoop.flink.io.reader.parsers.rawedges.NumberTokenizer;
 import org.gradoop.flink.io.reader.parsers.rawedges.RawEdgeFileParser;
 import org.gradoop.flink.model.impl.LogicalGraph;
 import org.gradoop.flink.model.impl.nested.datastructures.DataLake;
