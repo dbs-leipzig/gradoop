613fa2bd6ca620207966c010aaf320bfcfc94671
Stephan <stephan.ke@gmx.de>
2017-03-17 12:13:22 +0100

    Fixed problem where same vertex is added multiple times.

--- gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/grouping/functions/edgecentric/UpdateSuperVertexGroupItem.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/grouping/functions/edgecentric/UpdateSuperVertexGroupItem.java
@@ -19,6 +19,7 @@ package org.gradoop.flink.model.impl.operators.grouping.functions.edgecentric;
 
 import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.util.Collector;
+import org.gradoop.common.model.impl.id.GradoopId;
 import org.gradoop.common.model.impl.pojo.Vertex;
 import org.gradoop.flink.model.impl.operators.grouping.functions.BuildBase;
 import org.gradoop.flink.model.impl.operators.grouping.functions.aggregation.PropertyValueAggregator;
@@ -26,7 +27,9 @@ import org.gradoop.flink.model.impl.operators.grouping.tuples.vertexcentric.Edge
 import org.gradoop.flink.model.impl.operators.grouping.tuples.edgecentric.SuperVertexGroupItem;
 import org.gradoop.flink.model.impl.operators.grouping.tuples.edgecentric.SuperVertexIdWithVertex;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Takes an EPGM edge as input and creates a {@link EdgeGroupItem} which
@@ -41,6 +44,14 @@ public class UpdateSuperVertexGroupItem
 
   private final String LABEL_SEPARATOR = "_";
 
+  private Set<GradoopId> usedVertices;
+
+  private StringBuilder label;
+
+  private boolean isFirst;
+
+  private boolean duplicate;
+
   /**
    * Creates map function.
    *
@@ -51,6 +62,8 @@ public class UpdateSuperVertexGroupItem
   public UpdateSuperVertexGroupItem(List<String> groupPropertyKeys,
     boolean useLabel, List<PropertyValueAggregator> edgeAggregators) {
     super(groupPropertyKeys, useLabel, edgeAggregators);
+    usedVertices = new HashSet<>();
+    label = new StringBuilder();
   }
 
   @Override
@@ -60,27 +73,33 @@ public class UpdateSuperVertexGroupItem
     Collector<SuperVertexGroupItem> collector)
     throws Exception {
 
-    boolean isFirst;
-    StringBuilder label = new StringBuilder();
+    usedVertices.clear();
+    label.setLength(0);
+
     SuperVertexGroupItem superVertexGroupItem = superVertexGroupItems.iterator().next();
 
     isFirst = true;
     Vertex vertex;
+
     for (SuperVertexIdWithVertex superVertexIdWithVertex : superVertexIdWithVertices) {
       vertex = superVertexIdWithVertex.getVertex();
+      duplicate = usedVertices.contains(vertex.getId());
       if (useLabel()) {
-        if (!isFirst) {
-          label.append(LABEL_SEPARATOR);
+        if (!duplicate) {
+          if (!isFirst) {
+            label.append(LABEL_SEPARATOR);
+          }
+          label.append(getLabel(vertex));
         }
-        label.append(getLabel(vertex));
       }
-      if (doAggregate()) {
-        aggregate(getAggregateValues(vertex));
-      }
+        if (doAggregate() && !duplicate) {
+          aggregate(getAggregateValues(vertex));
+        }
       if (isFirst) {
         superVertexGroupItem.setGroupingValues(getGroupProperties(vertex));
         isFirst = false;
       }
+      usedVertices.add(vertex.getId());
     }
     superVertexGroupItem.setGroupLabel(label.toString());
     superVertexGroupItem.setAggregateValues(getAggregateValues());
