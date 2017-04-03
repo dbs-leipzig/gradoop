5a0511200114fa5f9e978b9e16e1001694430712
Stephan <stephan.ke@gmx.de>
2017-04-03 14:41:18 +0200

    [#204] Changed naming.

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/xmlbasedcsv/XMLBasedCSVBase.java
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.io.impl.xmlbasedcsv;
+
+import org.gradoop.flink.util.GradoopFlinkConfig;
+
+/**
+ * Base class for CSV data source and sink.
+ */
+abstract class XMLBasedCSVBase {
+  /**
+   * Gradoop Flink configuration
+   */
+  private final GradoopFlinkConfig config;
+  /**
+   * Complete path to the xsd file.
+   */
+  private final String xsdPath = XMLBasedCSVBase.class.getResource("/data/xmlbasedcsv/csv_format" +
+    ".xsd").getFile();
+  /**
+   * Complete path to the xml file.
+   */
+  private final String metaXmlPath;
+  /**
+   * Path to the directory containing the xmlbasedcsv files.
+   */
+  private String csvDir;
+
+  /**
+   * Creates a new data source/sink. Paths can be local (file://) or HDFS (hdfs://).
+   *
+   * @param metaXmlPath xml file
+   * @param csvDir xmlbasedcsv directory
+   * @param config Gradoop Flink configuration
+   */
+  XMLBasedCSVBase(String metaXmlPath, String csvDir, GradoopFlinkConfig config) {
+    if (config == null) {
+      throw new IllegalArgumentException("config must not be null");
+    }
+    if (metaXmlPath == null) {
+      throw new IllegalArgumentException("path to xml must not be null");
+    }
+    if (csvDir == null) {
+      throw new IllegalArgumentException("xmlbasedcsv directory must not be null");
+    }
+
+    this.config = config;
+    this.metaXmlPath = metaXmlPath;
+    this.csvDir = csvDir;
+  }
+
+  public GradoopFlinkConfig getConfig() {
+    return config;
+  }
+
+  public String getMetaXmlPath() {
+    return metaXmlPath;
+  }
+
+  public String getXsdPath() {
+    return xsdPath;
+  }
+
+  public String getCsvDir() {
+    return csvDir;
+  }
+}
---
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/xmlbasedcsv/pojos/CsvExtension.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.io.impl.xmlbasedcsv.pojos;
+
+/**
+ * This class is not part of the auto generated jaxb pojos so if needed modify with care.
+ * The extension to the generated class is needed because the datasource name and the domain name
+ * have to be saved together with the xmlbasedcsv meta information.
+ */
+public class CsvExtension extends Csv {
+    /**
+     * Name of the datasource where the, in xmlbasedcsv described, object is located in.
+     */
+    private String datasourceName;
+    /**
+     * Name of the domain where the, in xmlbasedcsv described, object is located in.
+     */
+    private String domainName;
+
+    public String getDatasourceName() {
+        return datasourceName;
+    }
+
+    public void setDatasourceName(String datasourceName) {
+        this.datasourceName = datasourceName;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+}
