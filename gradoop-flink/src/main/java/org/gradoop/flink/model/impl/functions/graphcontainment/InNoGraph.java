39e7d9f3a93b89d45140a1a6296e614644d3c522
p3et <petermann@informatik.uni-leipzig.de>
2017-06-22 18:51:22 +0200

    IR

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/functions/graphcontainment/InNoGraph.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of Gradoop.
+ *
+ * Gradoop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gradoop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gradoop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gradoop.flink.model.impl.functions.graphcontainment;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.gradoop.common.model.api.entities.EPGMGraphElement;
+
+/**
+ * True, if the element has not graph ids.
+ *
+ * @param <EL> epgm graph element
+ */
+public class InNoGraph<EL extends EPGMGraphElement> implements FilterFunction<EL> {
+
+  @Override
+  public boolean filter(EL value) throws Exception {
+    return value.getGraphIds() == null || value.getGraphIds().isEmpty();
+  }
+}
