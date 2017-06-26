b8e35c394f228f5b864e112c0bbe9d99df030855
Giacomo Bergami <giacomo@openmailbox.org>
2017-06-26 10:42:37 +0200

    Forgot the forwarding

--- gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/fusion/functions/LeftElementId.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/fusion/functions/LeftElementId.java
@@ -17,6 +17,7 @@
 
 package org.gradoop.flink.model.impl.operators.fusion.functions;
 
+import org.apache.flink.api.java.functions.FunctionAnnotation;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.gradoop.common.model.api.entities.EPGMElement;
@@ -27,6 +28,7 @@ import org.gradoop.common.model.impl.id.GradoopId;
  * @param <K> Element where the id is extracted
  *
  */
+@FunctionAnnotation.ForwardedFieldsFirst("id -> *")
 public class LeftElementId<K extends EPGMElement>
   implements KeySelector<Tuple2<K, GradoopId>, GradoopId> {
   @Override
