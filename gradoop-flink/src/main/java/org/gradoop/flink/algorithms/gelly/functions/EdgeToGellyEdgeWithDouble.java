d37ba245c7591acbf7edb5a15b87aec6e028d720
Caroline Mösler <caroline.moesler@gmx.de>
2018-02-08 13:38:21 +0100

    [fix]JUnit test for single source shortest paths is successful. Adapt mvn checkstyle to classes for single source shortest paths.

--- gradoop-flink/src/main/java/org/gradoop/flink/algorithms/gelly/functions/EdgeToGellyEdgeWithDouble.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/algorithms/gelly/functions/EdgeToGellyEdgeWithDouble.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2014 - 2017 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradoop.flink.algorithms.gelly.functions;
 
 import org.apache.flink.graph.Edge;
@@ -9,27 +24,32 @@ import org.gradoop.common.model.impl.id.GradoopId;
  */
 public class EdgeToGellyEdgeWithDouble implements EdgeToGellyEdge<Double> {
 
-	/**
-	 * Property key to get the value for.
-	 */
-	private final String propertyKey;
+  /**
+   * Property key to get the value for.
+   */
+  private final String propertyKey;
 
-	/**
-	 * Reduce object instantiations.
-	 */
-	private final org.apache.flink.graph.Edge<GradoopId, Double> reuseEdge;
+  /**
+   * Reduce object instantiations.
+   */
+  private final org.apache.flink.graph.Edge<GradoopId, Double> reuseEdge;
 
-	public EdgeToGellyEdgeWithDouble(String propertyKey) {
-		this.propertyKey = propertyKey;
-		this.reuseEdge = new org.apache.flink.graph.Edge<>();
-	}
+  /**
+   * Constructor.
+   *
+   * @param propertyKey property key to get the property value.
+   */
+  public EdgeToGellyEdgeWithDouble(String propertyKey) {
+    this.propertyKey = propertyKey;
+    this.reuseEdge = new org.apache.flink.graph.Edge<>();
+  }
 
-	@Override
-	public Edge<GradoopId, Double> map(org.gradoop.common.model.impl.pojo.Edge epgmEdge) throws Exception {
-		reuseEdge.setSource(epgmEdge.getSourceId());
-		reuseEdge.setTarget(epgmEdge.getTargetId());
-		reuseEdge.setValue(epgmEdge.getPropertyValue(propertyKey).getDouble());
-		return reuseEdge;
-	}
-
+  @Override
+  public Edge<GradoopId, Double> map(org.gradoop.common.model.impl.pojo.Edge epgmEdge)
+    throws Exception {
+    reuseEdge.setSource(epgmEdge.getSourceId());
+    reuseEdge.setTarget(epgmEdge.getTargetId());
+    reuseEdge.setValue(epgmEdge.getPropertyValue(propertyKey).getDouble());
+    return reuseEdge;
+  }
 }
--- gradoop-flink/src/test/java/org/gradoop/flink/algorithms/gelly/shortestpaths/SingleSourceShortestPathsTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/algorithms/gelly/shortestpaths/SingleSourceShortestPathsTest.java
@@ -1,63 +1,69 @@
+/**
+ * Copyright © 2014 - 2017 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradoop.flink.algorithms.gelly.shortestpaths;
 
-import java.util.List;
-
 import org.gradoop.common.model.impl.id.GradoopId;
-import org.gradoop.common.model.impl.pojo.Edge;
 import org.gradoop.common.model.impl.pojo.Vertex;
 import org.gradoop.flink.model.GradoopFlinkTestBase;
 import org.gradoop.flink.model.api.epgm.LogicalGraph;
-import org.gradoop.flink.model.api.functions.TransformationFunction;
 import org.gradoop.flink.util.FlinkAsciiGraphLoader;
 import org.junit.Test;
 
 public class SingleSourceShortestPathsTest extends GradoopFlinkTestBase {
+  @Test
+  public void testByElementData() throws Exception {
 
-	public TransformationFunction<Vertex> transformVertex;
-	
-	@Test
-	public void testByElementData() throws Exception {
-		
-		String graphs = "input[" +
-		"(v0 {id:0, vertexValue:0.0d})" +
-		"(v1 {id:1, vertexValue:NULL})" +
-		"(v2 {id:2, vertexValue:NULL})" +
-		"(v3 {id:3, vertexValue:NULL})" +
-		"(v4 {id:4, vertexValue:NULL})" +
-		"(v0)-[e0 {edgeValue:2.0d}]->(v1)" +
-		"(v0)-[e1 {edgeValue:7.0d}]->(v2)" +
-		"(v0)-[e2 {edgeValue:6.0d}]->(v3)" +
-		"(v1)-[e3 {edgeValue:3.0d}]->(v2)" +
-		"(v1)-[e4 {edgeValue:6.0d}]->(v4)" +
-		"(v2)-[e5 {edgeValue:5.0d}]->(v4)" +
-		"(v3)-[e6 {edgeValue:1.0d}]->(v4)" +
-		"]" +
-		"result[" +
-		"(v5 {id:0, vertexValue:0.0d})" +
-		"(v6 {id:1, vertexValue:2.0d})" +
-		"(v7 {id:2, vertexValue:5.0d})" +
-		"(v8 {id:3, vertexValue:6.0d})" +
-		"(v9 {id:4, vertexValue:7.0d})" +
-		"(v5)-[e0 {edgeValue:2.0d}]->(v6)" +
-		"(v5)-[e1 {edgeValue:7.0d}]->(v7)" +
-		"(v5)-[e2 {edgeValue:6.0d}]->(v8)" +
-		"(v6)-[e3 {edgeValue:3.0d}]->(v7)" +
-		"(v6)-[e4 {edgeValue:6.0d}]->(v9)" +
-		"(v7)-[e5 {edgeValue:5.0d}]->(v9)" +
-		"(v8)-[e6 {edgeValue:1.0d}]->(v9)" +
-		"]";
-		
-		FlinkAsciiGraphLoader loader = getLoaderFromString(graphs);
-		LogicalGraph input = loader.getLogicalGraphByVariable("input");
-		Vertex srcVertex = loader.getVertexByVariable("v0");
-		GradoopId srcVertexId = srcVertex.getId();
-		
-		LogicalGraph outputGraph = input.callForGraph(new SingleSourceShortestPaths(srcVertexId, "edgeValue", 10, "vertexValue"));
-		LogicalGraph expect = loader.getLogicalGraphByVariable("result");
-		
-		
-	
-		collectAndAssertTrue(outputGraph.equalsByElementData(expect));
-		
-	}
+    String graphs = "input[" +
+      "(v0 {id:0, vertexValue:0.0d})" +
+      "(v1 {id:1, vertexValue:NULL})" +
+      "(v2 {id:2, vertexValue:NULL})" +
+      "(v3 {id:3, vertexValue:NULL})" +
+      "(v4 {id:4, vertexValue:NULL})" +
+      "(v0)-[e0 {edgeValue:2.0d}]->(v1)" +
+      "(v0)-[e1 {edgeValue:7.0d}]->(v2)" +
+      "(v0)-[e2 {edgeValue:6.0d}]->(v3)" +
+      "(v1)-[e3 {edgeValue:3.0d}]->(v2)" +
+      "(v1)-[e4 {edgeValue:6.0d}]->(v4)" +
+      "(v2)-[e5 {edgeValue:5.0d}]->(v4)" +
+      "(v3)-[e6 {edgeValue:1.0d}]->(v4)" +
+      "]" +
+      "result[" +
+      "(v5 {id:0, vertexValue:0.0d})" +
+      "(v6 {id:1, vertexValue:2.0d})" +
+      "(v7 {id:2, vertexValue:5.0d})" +
+      "(v8 {id:3, vertexValue:6.0d})" +
+      "(v9 {id:4, vertexValue:7.0d})" +
+      "(v5)-[e7 {edgeValue:2.0d}]->(v6)" +
+      "(v5)-[e8 {edgeValue:7.0d}]->(v7)" +
+      "(v5)-[e9 {edgeValue:6.0d}]->(v8)" +
+      "(v6)-[e10 {edgeValue:3.0d}]->(v7)" +
+      "(v6)-[e11 {edgeValue:6.0d}]->(v9)" +
+      "(v7)-[e12 {edgeValue:5.0d}]->(v9)" +
+      "(v8)-[e13 {edgeValue:1.0d}]->(v9)" +
+      "]";
+
+    FlinkAsciiGraphLoader loader = getLoaderFromString(graphs);
+    LogicalGraph input = loader.getLogicalGraphByVariable("input");
+    Vertex srcVertex = loader.getVertexByVariable("v0");
+    GradoopId srcVertexId = srcVertex.getId();
+
+    LogicalGraph outputGraph = input.callForGraph(new SingleSourceShortestPaths(srcVertexId,
+      "edgeValue", 10, "vertexValue"));
+    LogicalGraph expect = loader.getLogicalGraphByVariable("result");
+
+    collectAndAssertTrue(outputGraph.equalsByElementData(expect));
+  }
 }
