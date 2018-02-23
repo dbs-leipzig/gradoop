666589ce5300e9929de45842d1d0363bc61f9e58
chpengzh <chpengzh@foxmail.com>
2018-02-23 23:03:01 +0800

    remove unsed try catch

--- gradoop-flink/src/test/java/org/gradoop/flink/io/impl/csv/indexed/IndexedCSVDataSourceTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/io/impl/csv/indexed/IndexedCSVDataSourceTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2014 - 2018 Leipzig University (Database Research Group)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,21 +25,16 @@ public class IndexedCSVDataSourceTest extends GradoopFlinkTestBase {
 
   @Test
   public void testRead() throws Exception {
-    try {
-      String csvPath =
-        VertexLabeledEdgeListDataSourceTest.class.getResource("/data/csv/input_indexed").getFile();
+    String csvPath =
+      VertexLabeledEdgeListDataSourceTest.class.getResource("/data/csv/input_indexed").getFile();
 
-      String gdlPath =
-        IndexedCSVDataSourceTest.class.getResource("/data/csv/expected/expected.gdl").getFile();
+    String gdlPath =
+      IndexedCSVDataSourceTest.class.getResource("/data/csv/expected/expected.gdl").getFile();
 
-      DataSource dataSource = new IndexedCSVDataSource(csvPath, getConfig());
-      LogicalGraph input = dataSource.getLogicalGraph();
-      LogicalGraph expected = getLoaderFromFile(gdlPath).getLogicalGraphByVariable("expected");
+    DataSource dataSource = new IndexedCSVDataSource(csvPath, getConfig());
+    LogicalGraph input = dataSource.getLogicalGraph();
+    LogicalGraph expected = getLoaderFromFile(gdlPath).getLogicalGraphByVariable("expected");
 
-      collectAndAssertTrue(input.equalsByElementData(expected));
-    } catch (Exception err ){
-      err.printStackTrace();
-      throw err;
-    }
+    collectAndAssertTrue(input.equalsByElementData(expected));
   }
 }
