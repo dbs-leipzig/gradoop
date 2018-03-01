378fd109d40b49ca8347c8d5803c9befdddd3df5
Caroline Mösler <caroline.moesler@gmx.de>
2018-03-01 12:40:50 +0100

    [feature]Create an IndexedCSVDataSink to write logicalgraphs indexed by label in csv data. Add junit test 'IndexedCSVDataSink.class'.

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/indexed/IndexedCSVDataSink.java
@@ -0,0 +1,183 @@
+/**
+ * Copyright © 2014 - 2018 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradoop.flink.io.impl.csv.indexed;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.core.fs.FileSystem;
+import org.gradoop.common.model.impl.pojo.Edge;
+import org.gradoop.common.model.impl.pojo.Element;
+import org.gradoop.common.model.impl.pojo.Vertex;
+import org.gradoop.flink.io.api.DataSink;
+import org.gradoop.flink.io.impl.csv.CSVBase;
+import org.gradoop.flink.io.impl.csv.CSVConstants;
+import org.gradoop.flink.io.impl.csv.functions.EdgeToCSVEdge;
+import org.gradoop.flink.io.impl.csv.functions.ElementToPropertyMetaData;
+import org.gradoop.flink.io.impl.csv.functions.ReducePropertyMetaData;
+import org.gradoop.flink.io.impl.csv.functions.VertexToCSVVertex;
+import org.gradoop.flink.io.impl.csv.metadata.MetaData;
+import org.gradoop.flink.io.impl.csv.metadata.MetaDataParser;
+import org.gradoop.flink.io.impl.csv.tuples.CSVEdge;
+import org.gradoop.flink.io.impl.csv.tuples.CSVVertex;
+import org.gradoop.flink.model.api.epgm.GraphCollection;
+import org.gradoop.flink.model.api.epgm.LogicalGraph;
+import org.gradoop.flink.util.GradoopFlinkConfig;
+
+/**
+ * A graph data sink for CSV files indexed by label.
+ */
+public class IndexedCSVDataSink extends CSVBase implements DataSink {
+
+  /**
+   * Path to meta data file that is used to write the output.
+   */
+  private final String metaDataPath;
+
+  /**
+   * Creates a new indexed CSV data sink. Computes the meta data based on the given graph.
+   *
+   * @param csvPath directory to write to
+   * @param config Gradoop Flink configuration
+   * @throws IOException
+   */
+  public IndexedCSVDataSink(String csvPath, GradoopFlinkConfig config) throws IOException {
+    this(csvPath, null, config);
+  }
+
+  /**
+   * Creates an new indexed CSV data sink. Uses the specified meta data to write the CSV output.
+   *
+   * @param csvPath directory to write CSV files to
+   * @param metaDataPath path to meta data CSV file
+   * @param config Gradoop Flink configuration
+   */
+  public IndexedCSVDataSink(String csvPath, String metaDataPath, GradoopFlinkConfig config)
+      throws IOException {
+    super(csvPath, config);
+    this.metaDataPath = metaDataPath;
+  }
+
+  @Override
+  public void write(LogicalGraph logicalGraph) throws IOException {
+    write(logicalGraph, false);
+  }
+
+  @Override
+  public void write(GraphCollection graphCollection) throws IOException {
+    write(graphCollection, false);
+  }
+
+  @Override
+  public void write(LogicalGraph logicalGraph, boolean overwrite) throws IOException {
+    FileSystem.WriteMode writeMode = overwrite ?
+        FileSystem.WriteMode.OVERWRITE : FileSystem.WriteMode.NO_OVERWRITE;
+
+    DataSet<Tuple2<String, String>> metaData;
+    if (!reuseMetadata()) {
+      metaData = createMetaData(logicalGraph);
+    } else {
+      metaData = MetaData.fromFile(metaDataPath, getConfig());
+    }
+
+    Map<String, DataSet<CSVVertex>> vertices = new HashMap<>();
+    Map<String, DataSet<CSVEdge>> edges = new HashMap<>();
+
+    if (logicalGraph.isGVELayout()) {
+      try {
+        for (Vertex v : logicalGraph.getVertices().collect()) {
+          if (!vertices.containsKey(v.getLabel())) {
+            vertices.put(v.getLabel(), logicalGraph.getVerticesByLabel(v.getLabel())
+                .map(new VertexToCSVVertex())
+                .withBroadcastSet(metaData, BC_METADATA));
+          }
+        }
+        for (Edge e : logicalGraph.getEdges().collect()) {
+          if (!edges.containsKey(e.getLabel())) {
+            edges.put(e.getLabel(), logicalGraph.getEdgesByLabel(e.getLabel())
+              .map(new EdgeToCSVEdge())
+              .withBroadcastSet(metaData, BC_METADATA));
+          }
+        }
+      } catch (Exception e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+
+    metaData.writeAsCsv(getMetaDataPath(), CSVConstants.ROW_DELIMITER,
+        CSVConstants.TOKEN_DELIMITER, writeMode).setParallelism(1);
+
+    for (Map.Entry<String, DataSet<CSVVertex>> vertex : vertices.entrySet()) {
+      vertex.getValue().writeAsCsv(getVertexCSVPath(vertex.getKey()), CSVConstants.ROW_DELIMITER,
+          CSVConstants.TOKEN_DELIMITER, writeMode);
+    }
+
+    for (Map.Entry<String, DataSet<CSVEdge>> edge : edges.entrySet()) {
+      edge.getValue().writeAsCsv(getEdgeCSVPath(edge.getKey()), CSVConstants.ROW_DELIMITER,
+         CSVConstants.TOKEN_DELIMITER, writeMode);
+    }
+  }
+
+  @Override
+  public void write(GraphCollection graphCollection, boolean overWrite) throws IOException {
+    // TODO Auto-generated method stub
+  }
+
+  /**
+   * Returns true, if the meta data shall be reused.
+   *
+   * @return true, iff reuse is possible
+   */
+  private boolean reuseMetadata() {
+    return this.metaDataPath != null && !this.metaDataPath.isEmpty();
+  }
+
+  /**
+   * Creates the meta data for the given graph.
+   *
+   * @param graph logical graph
+   * @return meta data information
+   */
+  private DataSet<Tuple2<String, String>> createMetaData(LogicalGraph graph) {
+    return createMetaData(graph.getVertices())
+      .union(createMetaData(graph.getEdges()));
+  }
+
+  /**
+   * Creates the meta data for the specified data set of EPGM elements.
+   *
+   * @param elements EPGM elements
+   * @param <E> EPGM element type
+   * @return meta data information
+   */
+  private <E extends Element> DataSet<Tuple2<String, String>> createMetaData(DataSet<E> elements) {
+    return elements
+      .map(new ElementToPropertyMetaData<>())
+      .groupBy(0)
+      .combineGroup(new ReducePropertyMetaData())
+      .groupBy(0)
+      .reduceGroup(new ReducePropertyMetaData())
+      .map(tuple -> Tuple2.of(tuple.f0, MetaDataParser.getPropertiesMetaData(tuple.f1)))
+      .returns(new TupleTypeInfo<>(BasicTypeInfo.STRING_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO))
+      .withForwardedFields("f0");
+  }
+}
