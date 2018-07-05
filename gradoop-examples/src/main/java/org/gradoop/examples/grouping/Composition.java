a263fb84a7f7d6e8e16edcc169c92020099b5515
galpha <k.gomez@freenet.de>
2018-07-05 12:30:27 +0200

    [#859] cleaned code arrangement in Composition class

--- gradoop-examples/src/main/java/org/gradoop/examples/grouping/Composition.java
+++ gradoop-examples/src/main/java/org/gradoop/examples/grouping/Composition.java
@@ -77,12 +77,14 @@ public class Composition extends AbstractRunner {
       });
 
     // group the transformed graph by users decade and apply several aggregate functions
-    LogicalGraph summary = transformed.groupBy(Collections.singletonList("decade"), Arrays
-        .asList(new CountAggregator("count"),
-          new MinAggregator("yob", "min_yob"),
-          new MaxAggregator("yob", "max_yob")), Collections.emptyList(),
-      Collections.singletonList(
-        new CountAggregator("count")), GroupingStrategy.GROUP_COMBINE);
+    LogicalGraph summary = transformed.groupBy(
+        Collections.singletonList("decade"),
+        Arrays.asList(new CountAggregator("count"),
+        new MinAggregator("yob", "min_yob"),
+        new MaxAggregator("yob", "max_yob")), Collections.emptyList(),
+        Collections.singletonList(
+          new CountAggregator("count")),
+        GroupingStrategy.GROUP_COMBINE);
 
     // use the decade as label information for the DOT sink
     summary = summary.transformVertices((current, modified) -> {
