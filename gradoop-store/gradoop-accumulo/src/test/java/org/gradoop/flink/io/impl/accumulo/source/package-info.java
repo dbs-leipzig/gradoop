5d43921b39a86d7eb30b95731985efb80da8ff63
galpha <k.gomez@freenet.de>
2018-07-18 11:19:33 +0200

    [#884] fixed license block + add javadoc to package-info in gradoop-store, gradoop-accumulo and gradoop-hbase module

--- gradoop-store/gradoop-accumulo/src/test/java/org/gradoop/flink/io/impl/accumulo/source/package-info.java
+++ gradoop-store/gradoop-accumulo/src/test/java/org/gradoop/flink/io/impl/accumulo/source/package-info.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright © 2014 - 2018 Leipzig University (Database Research Group)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.gradoop.flink.io.impl.accumulo.source;
