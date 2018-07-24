e335675f1e9777b72b8533c03d858a908753c3b7
Matthias Täschner <matthias.taeschner@posteo.de>
2018-07-24 10:20:29 +0200

    [#875] add Random Limited Degree Vertex Sampling (#879)
    * fixes #875

---
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/sampling/functions/VertexDegree.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2014 - 2018 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradoop.flink.model.impl.operators.sampling.functions;
+
+/**
+ * Keeps the types of vertex-degrees and their respective property names.
+ * Available degree-types: input (IN), output (OUT), sum of both (IN_OUT).
+ */
+public enum VertexDegree {
+  /**
+   * The input degree
+   */
+  IN("_InDegree"),
+  /**
+   * The output degree
+   */
+  OUT("_OutDegree"),
+  /**
+   * The sum of input and output degree
+   */
+  IN_OUT("_InOutDegree");
+
+  /**
+   * The property name of a vertex degree type
+   */
+  private String degreePropertyName;
+
+  /**
+   * Enum constructor for degree type with property name.
+   * @param degreePropertyName The property name for a degree type
+   */
+  VertexDegree(String degreePropertyName) {
+    this.degreePropertyName = degreePropertyName;
+  }
+
+  /**
+   * Get the property name for an instance of VertexDegree.
+   * @return The degree property name
+   */
+  public String getName() {
+    return degreePropertyName;
+  }
+}
