6d2a14277ec4bf4914b78084c05590c706ec2f4e
hr73vexy <hr73vexy@studserv.uni-leipzig.de>
2018-09-14 19:34:33 +0200

    further clean

--- gradoop-examples/src/main/java/org/gradoop/examples/io/RdbmsExample.java
+++ gradoop-examples/src/main/java/org/gradoop/examples/io/RdbmsExample.java
@@ -15,10 +15,6 @@
  */
 package org.gradoop.examples.io;
 
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import org.apache.flink.api.common.ProgramDescription;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.gradoop.flink.io.impl.csv.CSVDataSink;
@@ -27,25 +23,31 @@ import org.gradoop.flink.model.api.epgm.LogicalGraph;
 import org.gradoop.flink.util.GradoopFlinkConfig;
 
 /**
- * Example program that converts a given relational database 
- * into a {@link LogicalGraph} and stores the
- * resulting {@link LogicalGraph} as JSON into declared directory.
+ * Example program that converts a given relational database into a
+ * {@link LogicalGraph} and stores the resulting {@link LogicalGraph} as JSON
+ * into declared directory.
  */
 public class RdbmsExample implements ProgramDescription {
 
 	/**
 	 * Converts a relational database to an epgm graph
 	 *
-	 * @param args[0] Jdbc url 
-	 * @param args[1] Username of database user
-	 * @param args[2] Password of database user
-	 * @param args[3] Valid path to a fitting jdbc driver
-	 * @param args[4] Valid jdbc driver class name
-	 * @param args[5] Valid path to output directory
+	 * @param args[0]:
+	 *            Jdbc url
+	 * @param args[1]:
+	 *            User name of database user
+	 * @param args[2]:
+	 *            Password of database user
+	 * @param args[3]:
+	 *            Valid path to a fitting jdbc driver
+	 * @param args[4]:
+	 *            Valid jdbc driver class name
+	 * @param args[5]:
+	 *            Valid path to output directory
 	 */
 	public static void main(String[] args) throws Exception {
 
-		if(args.length != 6){
+		if (args.length != 6) {
 			throw new IllegalArgumentException(
 					"Please provide url, user, pasword, path to jdbc driver jar, jdbc driver class name, output directory");
 		}
@@ -63,40 +65,21 @@ public class RdbmsExample implements ProgramDescription {
 		GradoopFlinkConfig gfc = GradoopFlinkConfig.createConfig(env);
 
 		// create DataSource
-		RdbmsDataSource dataSource = new RdbmsDataSource(url,user,pw,jdbcDriverPath, jdbcDriverClassName,gfc);
+		RdbmsDataSource dataSource = new RdbmsDataSource(url, user, pw, jdbcDriverPath, jdbcDriverClassName, gfc);
 
 		// get logical graph of datasource
 		LogicalGraph schema = dataSource.getLogicalGraph();
 
 		// write conversion result to given path with timestamp and db name
-		schema.writeTo(new CSVDataSink(outputPath + "/" + getDateString() + urlParser(url),gfc));
-		
+		schema.writeTo(new CSVDataSink(outputPath, gfc));
+
 		// execute program
 		env.execute();
-	}	
-	
-	/**
-	 * Creates timestamp for output filename
-	 * @return Timestamp
-	 */
-	public static String getDateString(){
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date date = new Date();
-		return dateFormat.format(date);
-	}
-
-	/**
-	 * Provides the name of the connected database
-	 * @return Database name
-	 */
-	public static String urlParser(String url){
-		String dbName = url.replaceAll(".*/", "");
-		return dbName;
 	}
 
 	@Override
 	public String getDescription() {
-		return "DataImport for relational databases, implementing a relational database to epgm graph database conversation.";
+		return "Data import for relational databases, implementing a relational database to epgm graph database conversion.";
 	}
 
 }
