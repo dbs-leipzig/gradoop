d35ec9030446ceb96636e84fcc9fa6b041ac43f0
Lucas Schons <schons@studserv.uni-leipzig.de>
2018-12-04 14:28:54 +0100

    Add LongStrategy, modify IntegerStrategy

--- gradoop-common/src/main/java/org/gradoop/common/model/impl/properties/PropertyValueStrategy.java
+++ gradoop-common/src/main/java/org/gradoop/common/model/impl/properties/PropertyValueStrategy.java
@@ -42,6 +42,7 @@ public interface PropertyValueStrategy<T> {
       classStrategyMap.put(Boolean.class, new BooleanStrategy());
       classStrategyMap.put(Set.class, new SetStrategy());
       classStrategyMap.put(Integer.class, new IntegerStrategy());
+      classStrategyMap.put(Long.class, new LongStrategy());
 
       byteStrategyMap = new HashMap<>(classStrategyMap.size());
       for (PropertyValueStrategy strategy : classStrategyMap.values()) {
@@ -262,10 +263,11 @@ public interface PropertyValueStrategy<T> {
 
     @Override
     public Integer read(DataInputView inputView) throws IOException {
-      byte[] rawBytes = new byte[Bytes.SIZEOF_INT];
-
-      inputView.read(rawBytes);
-
+      int length = Bytes.SIZEOF_INT;
+      byte[] rawBytes = new byte[length];
+      for (int i  = 0; i < rawBytes.length; i++) {
+        rawBytes[i] = inputView.readByte();
+      }
       return Bytes.toInt(rawBytes);
     }
 
@@ -286,7 +288,7 @@ public interface PropertyValueStrategy<T> {
 
     @Override
     public Integer get(byte[] bytes) {
-      return Bytes.toInt(bytes);
+      return Bytes.toInt(bytes, PropertyValue.OFFSET);
     }
 
     @Override
@@ -299,6 +301,59 @@ public interface PropertyValueStrategy<T> {
       byte[] rawBytes = new byte[PropertyValue.OFFSET + Bytes.SIZEOF_INT];
       rawBytes[0] = getRawType();
       Bytes.putInt(rawBytes, PropertyValue.OFFSET, value);
+      return rawBytes;
+    }
+  }
+
+  class LongStrategy implements PropertyValueStrategy<Long> {
+
+    @Override
+    public boolean write(Long value, DataOutputView outputView) throws IOException {
+      byte[] rawBytes = getRawBytes(value);
+      outputView.write(rawBytes);
+      return true;
+    }
+
+    @Override
+    public Long read(DataInputView inputView) throws IOException {
+      int length = Bytes.SIZEOF_LONG;
+      byte[] rawBytes = new byte[length];
+      for (int i  = 0; i < rawBytes.length; i++) {
+        rawBytes[i] = inputView.readByte();
+      }
+      return Bytes.toLong(rawBytes);
+    }
+
+    @Override
+    public int compare(Long value, Long other) {
+      return Long.compare(value, other);
+    }
+
+    @Override
+    public boolean is(Object value) {
+      return value instanceof Long;
+    }
+
+    @Override
+    public Class<Long> getType() {
+      return Long.class;
+    }
+
+    @Override
+    public Long get(byte[] bytes) {
+      return Bytes.toLong(bytes, PropertyValue.OFFSET);
+    }
+
+    @Override
+    public Byte getRawType() {
+      return PropertyValue.TYPE_LONG;
+    }
+
+    @Override
+    public byte[] getRawBytes(Long value) {
+      byte[] rawBytes = new byte[PropertyValue.OFFSET + Bytes.SIZEOF_LONG];
+      rawBytes[0] = getRawType();
+      Bytes.putLong(rawBytes, PropertyValue.OFFSET, value);
       return rawBytes;
     }
   }
