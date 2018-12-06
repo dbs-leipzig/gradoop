91db342e0a29eeb26e8232f800865192ea7a2607
Philip Fritzsche <p-f@users.noreply.github.com>
2018-12-06 15:20:53 +0100

    [#1011] Test and minor changes for EdgeToVertex

--- gradoop-data-integration/src/test/java/org/gradoop/dataintegration/transformation/EdgeToVertexTest.java
+++ gradoop-data-integration/src/test/java/org/gradoop/dataintegration/transformation/EdgeToVertexTest.java
@@ -16,7 +16,12 @@
 package org.gradoop.dataintegration.transformation;
 
 import org.gradoop.flink.model.GradoopFlinkTestBase;
+import org.gradoop.flink.model.api.operators.UnaryGraphToGraphOperator;
+import org.gradoop.flink.model.impl.epgm.LogicalGraph;
+import org.gradoop.flink.util.FlinkAsciiGraphLoader;
 import org.junit.Test;
+
+import static org.junit.Assert.fail;
 
 
 /**
@@ -24,9 +29,73 @@ import org.junit.Test;
  */
 public class EdgeToVertexTest extends GradoopFlinkTestBase {
 
+  /**
+   * The loader with the graphs used in the tests.
+   */
+  private final FlinkAsciiGraphLoader loader = getLoaderFromString("input[" +
+    "(a:VertexA)-[e:edgeToTransform {testProp: 1, testProp2: \"\"}]->(b:VertexB)" +
+    "(a)-[e2:anotherEdge]->(b)" +
+    "]" +
+    "expected [" +
+    "(a)-[e]->(b)" +
+    "(a)-[e2]->(b)" +
+    "(a)-[:fromSource]->(:VertexFromEdge {testProp: 1, testProp2: \"\"})-[:toTarget]->(b)" +
+    "]");
 
+  /**
+   * Test the {@link EdgeToVertex} operator where one new vertex is created.
+   *
+   * @throws Exception when the execution in Flink fails.
+   */
   @Test
-  public void testDummy() {
+  public void testWithVertexCreation() throws Exception {
+    UnaryGraphToGraphOperator operator = new EdgeToVertex("edgeToTransform", "VertexFromEdge",
+      "fromSource", "toTarget");
+    LogicalGraph result = loader.getLogicalGraphByVariable("input").callForGraph(operator);
+    LogicalGraph expected = loader.getLogicalGraphByVariable("expected");
 
+    collectAndAssertTrue(result.equalsByElementData(expected));
+  }
+
+  /**
+   * Test the {@link EdgeToVertex} operator with its first parameter being {@code null}.
+   * This should not change anything.
+   *
+   * @throws Exception when the execution in Flink fails.
+   */
+  @Test
+  public void testWithoutVertexCreation() throws Exception {
+    UnaryGraphToGraphOperator operator = new EdgeToVertex(null, "newLabel", "fromSource",
+      "toTarget");
+    LogicalGraph result = loader.getLogicalGraphByVariable("input").callForGraph(operator);
+    LogicalGraph expected = loader.getLogicalGraphByVariable("input");
+
+    collectAndAssertTrue(result.equalsByElementData(expected));
+  }
+
+  /**
+   * Test if {@link EdgeToVertex#EdgeToVertex(String, String, String, String) the constructor}
+   * handles null-values as intended.
+   */
+  @Test
+  public void testForNullParameters() {
+    final String notNull = "";
+    // Should not throw Exception.
+    new EdgeToVertex(null, notNull, notNull, notNull);
+    try {
+      new EdgeToVertex(null, null, notNull, notNull);
+      fail("Second parameter was null.");
+    } catch (NullPointerException npe) {
+    }
+    try {
+      new EdgeToVertex(null, notNull, null, notNull);
+      fail("Third parameter was null.");
+    } catch (NullPointerException npe) {
+    }
+    try {
+      new EdgeToVertex(null, notNull, notNull, null);
+      fail("Forth parameter was null.");
+    } catch (NullPointerException npe) {
+    }
   }
 }
