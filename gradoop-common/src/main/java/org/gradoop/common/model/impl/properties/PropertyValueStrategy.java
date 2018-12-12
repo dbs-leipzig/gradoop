dfde1c6711d263a18440e0953244e70a6bc87876
Lucas Schons <schons@studserv.uni-leipzig.de>
2018-12-12 16:04:02 +0100

    Add MapStrategy

--- gradoop-common/src/main/java/org/gradoop/common/model/impl/properties/PropertyValueStrategy.java
+++ gradoop-common/src/main/java/org/gradoop/common/model/impl/properties/PropertyValueStrategy.java
@@ -16,7 +16,13 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface PropertyValueStrategy<T> {
 
@@ -59,6 +65,7 @@ public interface PropertyValueStrategy<T> {
       classStrategyMap.put(GradoopId.class, new GradoopIdStrategy());
       classStrategyMap.put(String.class, new StringStrategy());
       classStrategyMap.put(List.class, new ListStrategy());
+      classStrategyMap.put(Map.class, new MapStrategy());
 
       byteStrategyMap = new HashMap<>(classStrategyMap.size());
       for (PropertyValueStrategy strategy : classStrategyMap.values()) {
@@ -398,6 +405,142 @@ public interface PropertyValueStrategy<T> {
         outputStream.write(getRawType());
         for (PropertyValue entry : list) {
           entry.write(outputView);
+        }
+      } catch (IOException e) {
+        throw new RuntimeException("Error writing PropertyValue");
+      }
+
+      return byteStream.toByteArray();
+    }
+  }
+
+  class MapStrategy implements PropertyValueStrategy<Map> {
+
+    @Override
+    public boolean write(Map value, DataOutputView outputView) throws IOException {
+      byte[] rawBytes = getRawBytes(value);
+      byte type = rawBytes[0];
+
+      if (rawBytes.length > PropertyValue.LARGE_PROPERTY_THRESHOLD) {
+        type |= PropertyValue.FLAG_LARGE;
+      }
+      outputView.writeByte(type);
+      // Write length as an int if the "large" flag is set.
+      if ((type & PropertyValue.FLAG_LARGE) == PropertyValue.FLAG_LARGE) {
+        outputView.writeInt(rawBytes.length - PropertyValue.OFFSET);
+      } else {
+        outputView.writeShort(rawBytes.length - PropertyValue.OFFSET);
+      }
+
+      outputView.write(rawBytes, PropertyValue.OFFSET, rawBytes.length - PropertyValue.OFFSET);
+      return true;
+    }
+
+    @Override
+    public Map read(DataInputView inputView) throws IOException {
+      int length = inputView.readShort();
+
+      byte[] rawBytes = new byte[length];
+
+      inputView.read(rawBytes);
+
+      PropertyValue key;
+      PropertyValue value;
+
+      Map<PropertyValue, PropertyValue> map = new HashMap<>();
+
+      ByteArrayInputStream byteStream = new ByteArrayInputStream(rawBytes);
+      DataInputStream inputStream = new DataInputStream(byteStream);
+      DataInputView internalInputView = new DataInputViewStreamWrapper(inputStream);
+
+      try {
+        while (inputStream.available() > 0) {
+          value = new PropertyValue();
+          value.read(internalInputView);
+
+          key = new PropertyValue();
+          key.read(internalInputView);
+
+          map.put(key, value);
+        }
+      } catch (IOException e) {
+        throw new RuntimeException("Error reading PropertyValue");
+      }
+
+      return map;
+    }
+
+    @Override
+    public int compare(Map value, Map other) {
+      throw new UnsupportedOperationException(
+        "Method compareTo() is not supported for Map;"
+      );
+    }
+
+    @Override
+    public boolean is(Object value) {
+      return value instanceof Map;
+    }
+
+    @Override
+    public Class<Map> getType() {
+      return Map.class;
+    }
+
+    @Override
+    public Map get(byte[] bytes) {
+      PropertyValue key;
+      PropertyValue value;
+
+      Map<PropertyValue, PropertyValue> map = new HashMap<PropertyValue, PropertyValue>();
+
+      ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
+      DataInputStream inputStream = new DataInputStream(byteStream);
+      DataInputView inputView = new DataInputViewStreamWrapper(inputStream);
+
+      try {
+        if (inputStream.skipBytes(PropertyValue.OFFSET) != PropertyValue.OFFSET) {
+          throw new RuntimeException("Malformed entry in PropertyValue List");
+        }
+        while (inputStream.available() > 0) {
+          key = new PropertyValue();
+          key.read(inputView);
+
+          value = new PropertyValue();
+          value.read(inputView);
+
+          map.put(key, value);
+        }
+      } catch (IOException e) {
+        throw new RuntimeException("Error reading PropertyValue");
+      }
+
+      return map;
+    }
+
+    @Override
+    public Byte getRawType() {
+      return PropertyValue.TYPE_MAP;
+    }
+
+    @Override
+    public byte[] getRawBytes(Map value) {
+      Map<PropertyValue, PropertyValue> map = value;
+
+      int size =
+        map.keySet().stream().mapToInt(PropertyValue::byteSize).sum() +
+          map.values().stream().mapToInt(PropertyValue::byteSize).sum() +
+          PropertyValue.OFFSET;
+
+      ByteArrayOutputStream byteStream = new ByteArrayOutputStream(size);
+      DataOutputStream outputStream = new DataOutputStream(byteStream);
+      DataOutputView outputView = new DataOutputViewStreamWrapper(outputStream);
+
+      try {
+        outputStream.write(PropertyValue.TYPE_MAP);
+        for (Map.Entry<PropertyValue, PropertyValue> entry : map.entrySet()) {
+          entry.getKey().write(outputView);
+          entry.getValue().write(outputView);
         }
       } catch (IOException e) {
         throw new RuntimeException("Error writing PropertyValue");
