89070250b61a473632a65bef1463c5cf96da9ba0
Matthias Täschner <matthias.taeschner@posteo.de>
2019-01-02 13:01:01 +0100

    Checkstyle: incorrect indentation level 4 - changed to 6

--- gradoop-common/src/main/java/org/gradoop/common/model/impl/id/GradoopId.java
+++ gradoop-common/src/main/java/org/gradoop/common/model/impl/id/GradoopId.java
@@ -86,8 +86,8 @@ public class GradoopId implements NormalizableKey<GradoopId>, CopyableValue<Grad
    * Required for {@link GradoopId#toString()}
    */
   private static final char[] HEX_CHARS = new char[] {
-    '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+      '0', '1', '2', '3', '4', '5', '6', '7',
+      '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
   /**
    * Internal byte representation
