3c2c5d00e9dbb65597b2a43547c7b39f5cd4ab50
timo95 <timo95@users.noreply.github.com>
2019-03-22 16:08:59 +0100

    [#1189] Remove untyped complex type support and refactor

--- gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/metadata/CSVMetaDataParser.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/metadata/CSVMetaDataParser.java
@@ -39,49 +39,32 @@ import java.util.stream.Collectors;
  */
 public class CSVMetaDataParser {
   /**
-   * Used to map a type string to its corresponding parsing function
+   * Used to map a simple type string to its corresponding parsing function.
    */
-  private static final Map<String, Function<String, Object>> TYPE_PARSER_MAP = getTypeParserMap();
+  private static final Map<String, Function<String, Object>> SIMPLE_TYPE_PARSER_MAP =
+    getSimpleTypeParserMap();
 
   /**
-   * Creates the type - parser function mapping of static property TYPE_PARSER_MAP
+   * Creates the type - parser function mapping of static property SIMPLE_TYPE_PARSER_MAP
    *
-   * @return a HashMap containing the mapping of a type string to its corresponding parsing function
+   * @return a HashMap containing the mapping of a simple type string to its corresponding
+   * parsing function.
    */
-  private static Map<String, Function<String, Object>> getTypeParserMap() {
+  private static Map<String, Function<String, Object>> getSimpleTypeParserMap() {
     Map<String, Function<String, Object>> map = new HashMap<>();
-    map.put(MetaData.TypeString.SHORT.getTypeString(),
-      Short::parseShort);
-    map.put(MetaData.TypeString.INTEGER.getTypeString(),
-      Integer::parseInt);
-    map.put(MetaData.TypeString.LONG.getTypeString(),
-      Long::parseLong);
-    map.put(MetaData.TypeString.FLOAT.getTypeString(),
-      Float::parseFloat);
-    map.put(MetaData.TypeString.DOUBLE.getTypeString(),
-      Double::parseDouble);
-    map.put(MetaData.TypeString.BOOLEAN.getTypeString(),
-      Boolean::parseBoolean);
-    map.put(MetaData.TypeString.STRING.getTypeString(),
-      CSVMetaDataParser::parseStringProperty);
-    map.put(MetaData.TypeString.BIGDECIMAL.getTypeString(),
-      BigDecimal::new);
-    map.put(MetaData.TypeString.GRADOOPID.getTypeString(),
-      GradoopId::fromString);
-    map.put(MetaData.TypeString.MAP.getTypeString(),
-      CSVMetaDataParser::parseMapProperty);
-    map.put(MetaData.TypeString.LIST.getTypeString(),
-      CSVMetaDataParser::parseListProperty);
-    map.put(MetaData.TypeString.LOCALDATE.getTypeString(),
-      LocalDate::parse);
-    map.put(MetaData.TypeString.LOCALTIME.getTypeString(),
-      LocalTime::parse);
-    map.put(MetaData.TypeString.LOCALDATETIME.getTypeString(),
-      LocalDateTime::parse);
-    map.put(MetaData.TypeString.NULL.getTypeString(),
-      CSVMetaDataParser::parseNullProperty);
-    map.put(MetaData.TypeString.SET.getTypeString(),
-      CSVMetaDataParser::parseSetProperty);
+    map.put(MetaData.TypeString.SHORT.getTypeString(), Short::parseShort);
+    map.put(MetaData.TypeString.INTEGER.getTypeString(), Integer::parseInt);
+    map.put(MetaData.TypeString.LONG.getTypeString(), Long::parseLong);
+    map.put(MetaData.TypeString.FLOAT.getTypeString(), Float::parseFloat);
+    map.put(MetaData.TypeString.DOUBLE.getTypeString(), Double::parseDouble);
+    map.put(MetaData.TypeString.BOOLEAN.getTypeString(), Boolean::parseBoolean);
+    map.put(MetaData.TypeString.STRING.getTypeString(), StringEscaper::unescape);
+    map.put(MetaData.TypeString.BIGDECIMAL.getTypeString(), BigDecimal::new);
+    map.put(MetaData.TypeString.GRADOOPID.getTypeString(), GradoopId::fromString);
+    map.put(MetaData.TypeString.LOCALDATE.getTypeString(), LocalDate::parse);
+    map.put(MetaData.TypeString.LOCALTIME.getTypeString(), LocalTime::parse);
+    map.put(MetaData.TypeString.LOCALDATETIME.getTypeString(), LocalDateTime::parse);
+    map.put(MetaData.TypeString.NULL.getTypeString(), CSVMetaDataParser::parseNullProperty);
     return Collections.unmodifiableMap(map);
   }
 
@@ -106,114 +89,94 @@ public class CSVMetaDataParser {
    * @return parsing function for the specific type
    */
   static Function<String, Object> getPropertyValueParser(String propertyType) {
-    String[] propertyTypeTokens = StringEscaper.split(
-      propertyType, PropertyMetaData.PROPERTY_TOKEN_DELIMITER);
-    if (propertyTypeTokens.length == 2 &&
-      propertyTypeTokens[0].equals(MetaData.TypeString.LIST.getTypeString())) {
-      // It's a list with one additional data type (type of list items).
-      return getListValueParser(propertyTypeTokens[1]);
-    } else if (propertyTypeTokens.length == 2 &&
-      propertyTypeTokens[0].equals(MetaData.TypeString.SET.getTypeString())) {
-      // It's a set with one additional data type (type of set items).
-      return getSetValueParser(propertyTypeTokens[1]);
-    } else if (propertyTypeTokens.length == 3 &&
-      propertyTypeTokens[0].equals(MetaData.TypeString.MAP.getTypeString())) {
-      // It's a map with two additional data types (key type + value type).
-      return getMapValueParser(propertyTypeTokens[1], propertyTypeTokens[2]);
-    } else {
-      return getValueParser(propertyType);
-    }
-  }
+    String[] typeTokens = StringEscaper.split(
+      propertyType.toLowerCase(), PropertyMetaData.PROPERTY_TOKEN_DELIMITER);
+    String mainType = typeTokens[0];
 
-  /**
-   * Creates a parsing function for the given primitive property type.
-   *
-   * @param type property type
-   * @return parsing function
-   */
-  private static Function<String, Object> getValueParser(String type) {
-    type = type.toLowerCase();
-    if (TYPE_PARSER_MAP.containsKey(type)) {
-      return TYPE_PARSER_MAP.get(type);
+    if (mainType.equals(MetaData.TypeString.LIST.getTypeString())) {
+      return getListValueParser(typeTokens);
+    } else if (mainType.equals(MetaData.TypeString.SET.getTypeString())) {
+      return getSetValueParser(typeTokens);
+    } else if (mainType.equals(MetaData.TypeString.MAP.getTypeString())) {
+      return getMapValueParser(typeTokens);
+    } else if (SIMPLE_TYPE_PARSER_MAP.containsKey(mainType)) {
+      return SIMPLE_TYPE_PARSER_MAP.get(mainType);
     } else {
-      throw new IllegalArgumentException("Type " + type + " is not supported");
+      throw new TypeNotPresentException(mainType, null);
     }
   }
 
   /**
    * Creates a parsing function for list property type.
    *
-   * @param listItemType string representation of the list item type, e.g. "String"
+   * @param listTypeTokens string tokens of the list type and its items type, e.g.
+   *                       ["list", "string"]
    * @return parsing function
    */
-  private static Function<String, Object> getListValueParser(String listItemType) {
-    final String itemType = listItemType.toLowerCase();
+  private static Function<String, Object> getListValueParser(String[] listTypeTokens) {
+    // It's a list with one additional data type (type of list items).
+    if (listTypeTokens.length != 2) {
+      throw new IllegalArgumentException("Item type of List type is missing");
+    }
+    final String itemType = listTypeTokens[1];
     // check the validity of the list item type
-    if (!TYPE_PARSER_MAP.containsKey(itemType)) {
+    if (!SIMPLE_TYPE_PARSER_MAP.containsKey(itemType)) {
       throw new TypeNotPresentException(itemType, null);
     }
 
-    return s -> parseListProperty(s, TYPE_PARSER_MAP.get(itemType));
+    return s -> parseListProperty(s, SIMPLE_TYPE_PARSER_MAP.get(itemType));
   }
 
   /**
    * Creates a parsing function for map property type.
    *
-   * @param keyType   string representation of the map key type, e.g. "String"
-   * @param valueType string representation of the map value type, e.g. "Double"
+   * @param mapTypeTokens string tokens of the map type and its key type and value type, e.g.
+   *                      ["map", "string", "double"]
    * @return parsing function
    */
-  private static Function<String, Object> getMapValueParser(String keyType, String valueType) {
-    final String keyTypeLowerCase = keyType.toLowerCase();
-    // check the validity of the map key type
-    if (!TYPE_PARSER_MAP.containsKey(keyTypeLowerCase)) {
-      throw new TypeNotPresentException(keyTypeLowerCase, null);
+  private static Function<String, Object> getMapValueParser(String[] mapTypeTokens) {
+    // It's a map with two additional data types (key type + value type).
+    if (mapTypeTokens.length != 3) {
+      throw new IllegalArgumentException("Key type or value type of Map type is missing");
     }
 
-    final String valueTypeLowerCase = valueType.toLowerCase();
+    final String keyType = mapTypeTokens[1];
+    // check the validity of the map key type
+    if (!SIMPLE_TYPE_PARSER_MAP.containsKey(keyType)) {
+      throw new TypeNotPresentException(keyType, null);
+    }
+
+    final String valueType = mapTypeTokens[2];
     // check the validity of the map value type
-    if (!TYPE_PARSER_MAP.containsKey(valueTypeLowerCase)) {
-      throw new TypeNotPresentException(valueTypeLowerCase, null);
+    if (!SIMPLE_TYPE_PARSER_MAP.containsKey(valueType)) {
+      throw new TypeNotPresentException(valueType, null);
     }
 
     return s -> parseMapProperty(
       s,
-      TYPE_PARSER_MAP.get(keyTypeLowerCase),
-      TYPE_PARSER_MAP.get(valueTypeLowerCase)
+      SIMPLE_TYPE_PARSER_MAP.get(keyType),
+      SIMPLE_TYPE_PARSER_MAP.get(valueType)
     );
   }
 
   /**
    * Creates a parsing function for set property type.
    *
-   * @param setItemType string representation of the set item type, e.g. "String"
+   * @param setTypeTokens string tokens of the set type and its item type, e.g. ["set", "string"]
    * @return parsing function
    */
-  private static Function<String, Object> getSetValueParser(String setItemType) {
-    final String itemType = setItemType.toLowerCase();
+  private static Function<String, Object> getSetValueParser(String[] setTypeTokens) {
+    // It's a set with one additional data type (type of set items).
+    if (setTypeTokens.length != 2) {
+      throw new IllegalArgumentException("Item type of Set type is missing");
+    }
+    final String itemType = setTypeTokens[1];
     // check the validity of the set item type
-    if (!TYPE_PARSER_MAP.containsKey(itemType)) {
+    if (!SIMPLE_TYPE_PARSER_MAP.containsKey(itemType)) {
       throw new TypeNotPresentException(itemType, null);
     }
 
-    return s -> parseSetProperty(s, TYPE_PARSER_MAP.get(itemType));
-  }
-
-  /**
-   * Parse function to translate string representation of a List to a list of PropertyValues
-   * Every PropertyValue has the type "string", because there is no parser for the items given
-   * Use {@link #parseListProperty(String, Function)} to specify a parsing function
-   *
-   * @param s the string to parse as list, e.g. "[myString1,myString2]"
-   * @return the list represented by the argument
-   */
-  private static Object parseListProperty(String s) {
-    // no item type given, so use string as type
-    s = s.substring(1, s.length() - 1);
-    return Arrays.stream(StringEscaper.split(s, CSVConstants.LIST_DELIMITER))
-      .map(StringEscaper::unescape)
-      .map(PropertyValue::create)
-      .collect(Collectors.toList());
+    return s -> parseSetProperty(s, SIMPLE_TYPE_PARSER_MAP.get(itemType));
   }
 
   /**
@@ -229,24 +192,6 @@ public class CSVMetaDataParser {
       .map(itemParser)
       .map(PropertyValue::create)
       .collect(Collectors.toList());
-  }
-
-  /**
-   * Parse function to translate string representation of a Map to a Map with key and value of
-   * type PropertyValue. Every PropertyValue (key and value) has the type "string", because there
-   * are no parsers for the keys and values given. Use
-   * {@link #parseMapProperty(String, Function, Function)} to specify both parsing functions.
-   *
-   * @param s the string to parse as map, e.g. "{myString1=myValue1,myString2=myValue2}"
-   * @return the map represented by the argument
-   */
-  private static Object parseMapProperty(String s) {
-    // no key type and value type given, so use string as types
-    s = s.substring(1, s.length() - 1);
-    return Arrays.stream(StringEscaper.split(s, CSVConstants.LIST_DELIMITER))
-      .map(st -> StringEscaper.split(st, CSVConstants.MAP_SEPARATOR))
-      .collect(Collectors.toMap(e -> PropertyValue.create(StringEscaper.unescape(e[0])),
-        e -> PropertyValue.create(StringEscaper.unescape(e[1]))));
   }
 
   /**
@@ -269,23 +214,6 @@ public class CSVMetaDataParser {
 
   /**
    * Parse function to translate string representation of a Set to a set of PropertyValues.
-   * Every PropertyValue has the type "string", because there is no parser for the items given.
-   * Use {@link #parseListProperty(String, Function)} to specify a parsing function.
-   *
-   * @param s the string to parse as set, e.g. "[myString1,myString2]"
-   * @return the set represented by the argument
-   */
-  private static Object parseSetProperty(String s) {
-    // no item type given, so use string as type
-    s = s.substring(1, s.length() - 1);
-    return Arrays.stream(StringEscaper.split(s, CSVConstants.LIST_DELIMITER))
-      .map(StringEscaper::unescape)
-      .map(PropertyValue::create)
-      .collect(Collectors.toSet());
-  }
-
-  /**
-   * Parse function to translate string representation of a Set to a set of PropertyValues.
    *
    * @param s          the string to parse as set, e.g. "[myString1,myString2]"
    * @param itemParser the function to parse the set items
@@ -297,16 +225,6 @@ public class CSVMetaDataParser {
       .map(itemParser)
       .map(PropertyValue::create)
       .collect(Collectors.toSet());
-  }
-
-  /**
-   * Parse function to translate CSV strings to strings.
-   *
-   * @param s the string to parse
-   * @return the unescaped string
-   */
-  private static Object parseStringProperty(String s) {
-    return StringEscaper.unescape(s);
   }
 
   /**
