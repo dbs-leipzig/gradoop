a9c480b0d65838ff8f54fdbaf2dee65cff8bf4bc
Timo Adameit <timo95@users.noreply.github.com>
2019-04-24 15:20:14 +0200

    [#1220] Fix style

--- gradoop-data-integration/src/main/java/org/gradoop/dataintegration/transformation/ConnectNeighbors.java
+++ gradoop-data-integration/src/main/java/org/gradoop/dataintegration/transformation/ConnectNeighbors.java
@@ -94,7 +94,7 @@ public class ConnectNeighbors implements UnaryGraphToGraphOperator {
       .map(new AddToGraphBroadcast<>())
       .withBroadcastSet(graph.getGraphHead().map(new Id<>()), AddToGraphBroadcast.GRAPH_ID);
 
-     return graph.getFactory()
+    return graph.getFactory()
       .fromDataSets(graph.getGraphHead(), graph.getVertices(), graph.getEdges().union(newEdges));
   }
 
