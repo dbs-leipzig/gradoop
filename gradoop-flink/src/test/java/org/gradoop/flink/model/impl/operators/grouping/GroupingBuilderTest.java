12a34ea243495517f91ef2c910de673ac04fb0c8
Julian Götz <goetz@infai.org>
2019-07-02 11:19:58 +0200

    #1305 added tests to check correct setting of grouping properties

--- gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
@@ -16,32 +16,18 @@
 package org.gradoop.flink.model.impl.operators.grouping;
 
 import com.google.common.collect.ImmutableList;
+import org.gradoop.common.model.impl.pojo.EPGMEdge;
+import org.gradoop.common.model.impl.pojo.EPGMGraphHead;
+import org.gradoop.common.model.impl.pojo.EPGMVertex;
+import org.gradoop.flink.model.impl.epgm.GraphCollection;
+import org.gradoop.flink.model.impl.epgm.LogicalGraph;
+import org.gradoop.flink.model.impl.operators.grouping.tuples.LabelGroup;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class GroupingBuilderTest {
-
-  /**
-   * Tests {@link Grouping.GroupingBuilder#build()}, expects a correct exception when not
-   * grouping by anything.
-   */
-  @Test(expected = IllegalStateException.class)
-  public void testGroupByNothingError() {
-
-    new Grouping.GroupingBuilder()
-      .build();
-  }
-
-  /**
-   * Tests {@link Grouping.GroupingBuilder#build()}, expects a correct exception when not
-   * grouping by anything.
-   */
-  @Test(expected = IllegalStateException.class)
-  public void testGroupByNothingError2() {
-
-    new Grouping.GroupingBuilder()
-      .setStrategy(GroupingStrategy.GROUP_REDUCE)
-      .build();
-  }
 
   /**
    * Tests {@link Grouping.GroupingBuilder#build()}, expects a correct exception when no
@@ -60,38 +46,111 @@ public class GroupingBuilderTest {
    * when only grouping by vertex labels.
    */
   @Test
-  public void testStrategySet() {
+  public void testGroupByVertexLabels() {
 
-    new Grouping.GroupingBuilder()
+    Grouping grouping = new Grouping.GroupingBuilder()
       .useVertexLabel(true)
       .setStrategy(GroupingStrategy.GROUP_REDUCE)
       .build();
+
+    Assert.assertTrue(grouping.useVertexLabels());
+  }
+
+  /**
+   * Tests successful call to {@link Grouping.GroupingBuilder#build()} when a strategy is set and
+   * when only grouping by vertex labels.
+   */
+  @Test
+  public void testGroupByEdgeLabels() {
+
+    Grouping grouping = new Grouping.GroupingBuilder()
+      .useEdgeLabel(true)
+      .setStrategy(GroupingStrategy.GROUP_REDUCE)
+      .build();
+
+    Assert.assertTrue(grouping.useEdgeLabels());
   }
 
   /**
    * Tests successful call to {@link Grouping.GroupingBuilder#build()} when only grouping by
-   * one property.
+   * one vertex property.
    */
   @Test
-  public void testAtLeastOneProperty() {
+  public void testGroupByVertexProperty() {
 
-    new Grouping.GroupingBuilder()
+    Grouping grouping = new Grouping.GroupingBuilder()
       .setStrategy(GroupingStrategy.GROUP_REDUCE)
       .addVertexGroupingKey("")
       .build();
+
+    Assert.assertTrue(grouping.useVertexProperties());
   }
 
   /**
-   * Tests successful call to {@link Grouping.GroupingBuilder#build()} when only using specific
-   * label grouping.
+   * Tests successful call to {@link Grouping.GroupingBuilder#build()} when only grouping by
+   * one edge property.
    */
   @Test
-  public void testLabelSpecificGroup() {
+  public void testGroupByEdgeProperty() {
 
-    new Grouping.GroupingBuilder()
+    Grouping grouping = new Grouping.GroupingBuilder()
       .setStrategy(GroupingStrategy.GROUP_REDUCE)
-      .addVertexLabelGroup("", ImmutableList.of())
+      .addEdgeGroupingKey("")
       .build();
+
+    Assert.assertTrue(grouping.useEdgeProperties());
+  }
+
+  /**
+   * Tests successful call to {@link Grouping.GroupingBuilder#build()} when using specific
+   * vertex label grouping.
+   */
+  @Test
+  public void testVertexLabelSpecificGroup() {
+
+    String testGroupLabel = "testGroup";
+    ImmutableList<String> testGroupingKeys = ImmutableList.of("a", "b");
+
+    Grouping<EPGMGraphHead, EPGMVertex, EPGMEdge, LogicalGraph, GraphCollection> grouping =
+      new Grouping.GroupingBuilder()
+        .setStrategy(GroupingStrategy.GROUP_REDUCE)
+        .addVertexLabelGroup(testGroupLabel, testGroupingKeys)
+        .build();
+
+    List<LabelGroup> vertexLabelGroups = grouping.getVertexLabelGroups();
+
+    // first element of vertexLabelGroups is always the defaultVertexLabelGroup, so the label
+    // specific group should be at index 1.
+    LabelGroup group = vertexLabelGroups.get(1);
+
+    Assert.assertEquals(group.getGroupLabel(), testGroupLabel);
+    Assert.assertEquals(testGroupingKeys, group.getPropertyKeys());
+  }
+
+  /**
+   * Tests successful call to {@link Grouping.GroupingBuilder#build()} when using specific
+   * edge label grouping.
+   */
+  @Test
+  public void testEdgeLabelSpecificGroup() {
+
+    String testGroupLabel = "testGroup";
+    ImmutableList<String> testGroupingKeys = ImmutableList.of("a", "b");
+
+    Grouping<EPGMGraphHead, EPGMVertex, EPGMEdge, LogicalGraph, GraphCollection> grouping =
+      new Grouping.GroupingBuilder()
+        .setStrategy(GroupingStrategy.GROUP_REDUCE)
+        .addEdgeLabelGroup(testGroupLabel, testGroupingKeys)
+        .build();
+
+    List<LabelGroup> edgeLabelGroups = grouping.getEdgeLabelGroups();
+
+    // first element of edgeLabelGroups is always the defaultEdgeLabelGroup, so the label
+    // specific group should be at index 1.
+    LabelGroup group = edgeLabelGroups.get(1);
+
+    Assert.assertEquals(group.getGroupLabel(), testGroupLabel);
+    Assert.assertEquals(testGroupingKeys, group.getPropertyKeys());
   }
 
 }
