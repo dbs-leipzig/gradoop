9226d9ee8478144b3a179527cda684a9e47073e6
Julian G <juliangoetz632@gmail.com>
2019-07-05 10:21:15 +0200

    Update gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
    Co-Authored-By: Christopher Rost <18614530+ChrizZz110@users.noreply.github.com>

--- gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
@@ -27,7 +27,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 /**
