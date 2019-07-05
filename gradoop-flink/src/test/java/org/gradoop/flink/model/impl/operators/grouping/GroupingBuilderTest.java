818d1ab303bbf85e61d42d4cc3147da4a469278f
Julian G <juliangoetz632@gmail.com>
2019-07-05 10:21:44 +0200

    Update gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
    Co-Authored-By: Christopher Rost <18614530+ChrizZz110@users.noreply.github.com>

--- gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/grouping/GroupingBuilderTest.java
@@ -118,7 +118,7 @@ public class GroupingBuilderTest {
       .addVertexGroupingKey("")
       .build();
 
-    Assert.assertTrue(grouping.useVertexProperties());
+    assertTrue(grouping.useVertexProperties());
   }
 
   /**
