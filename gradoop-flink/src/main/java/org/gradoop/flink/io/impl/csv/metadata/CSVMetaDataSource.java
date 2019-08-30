0c2f8d12be00984435690af263e5286e4f78be47
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-08-30 14:53:08 +0200

    [#1383] Replace usage of Charset.forName with StandardCharsets

--- gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/metadata/CSVMetaDataSource.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/io/impl/csv/metadata/CSVMetaDataSource.java
@@ -32,6 +32,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -101,7 +102,7 @@ public class CSVMetaDataSource implements MetaDataSource<CSVMetaData> {
   public CSVMetaData readLocal(String path, Configuration hdfsConfig) throws IOException {
     FileSystem fs = FileSystem.get(hdfsConfig);
     Path file = new Path(path);
-    Charset charset = Charset.forName("UTF-8");
+    Charset charset = StandardCharsets.UTF_8;
 
     if (!fs.exists(file)) {
       throw new FileNotFoundException(path);
