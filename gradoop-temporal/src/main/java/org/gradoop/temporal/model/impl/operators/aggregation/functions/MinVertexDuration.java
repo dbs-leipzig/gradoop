02e02c2b894c9107e6e631398928b2d60cff1d0a
xnqio <derserafin@gmail.com>
2019-10-30 10:25:49 +0100

    [#1414] created Min and MaxDuration aggregate functions equivalent to the average Duration function and tested them in the MinMaxDurationTest class

---
+++ gradoop-temporal/src/main/java/org/gradoop/temporal/model/impl/operators/aggregation/functions/MinVertexDuration.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2014 - 2019 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradoop.temporal.model.impl.operators.aggregation.functions;
+
+import org.gradoop.flink.model.api.functions.VertexAggregateFunction;
+import org.gradoop.temporal.model.api.TimeDimension;
+
+/**
+ * Calculates the minimum duration of all given vertices and the defined {@link TimeDimension}
+ */
+public class MinVertexDuration extends MinDuration implements VertexAggregateFunction {
+
+  /**
+   * Creates a new instance of a base aggregate function.
+   *
+   * @param aggregatePropertyKey aggregate property key
+   * @param dimension the given TimeDimension
+   */
+  public MinVertexDuration(String aggregatePropertyKey, TimeDimension dimension) {
+    super(aggregatePropertyKey, dimension);
+  }
+}
