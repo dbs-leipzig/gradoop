dc45fbb016e156bfc2b5a02ffde06d09217b2b21
timo95 <timo95@users.noreply.github.com>
2021-02-26 12:14:27 +0100

    Debug strings

--- gradoop-flink/src/test/java/org/gradoop/flink/io/impl/csv/CSVDataSinkTest.java
+++ gradoop-flink/src/test/java/org/gradoop/flink/io/impl/csv/CSVDataSinkTest.java
@@ -266,6 +266,9 @@ public class CSVDataSinkTest extends CSVTestBase {
   @Test
   public void testWriteMetadataCsvLocal() throws Exception {
     String tmpPath = temporaryFolder.getRoot().getPath();
+
+    System.out.println(System.getProperty("os.name"));
+
     LogicalGraph logicalGraph = getExtendedLogicalGraph();
 
     List<Tuple3<String, String, String>> metaDataTuples = new CSVMetaDataSource()
