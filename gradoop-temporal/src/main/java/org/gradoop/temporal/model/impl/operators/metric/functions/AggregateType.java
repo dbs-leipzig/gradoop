807ec2d789657dd4b50ea3f72259ecafc1eeee8b
Alwine Balfanz <100916390+alwba@users.noreply.github.com>
2022-08-02 11:05:58 +0200

    [#1559] add help functions

---
+++ gradoop-temporal/src/main/java/org/gradoop/temporal/model/impl/operators/metric/functions/AggregateType.java
@@ -0,0 +1,19 @@
+package org.gradoop.temporal.model.impl.operators.metric.functions;
+
+/**
+ * Enum for defining an aggregate type.
+ */
+public enum AggregateType {
+    /**
+     * Minimum aggregation.
+     */
+    MIN,
+    /**
+     * Maximum aggregation.
+     */
+    MAX,
+    /**
+     * Average aggregation.
+     */
+    AVG
+}
