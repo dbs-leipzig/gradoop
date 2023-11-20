0b6a5c55bf3e92dd47e983f0c70a2ec7ac30207a
Chris <christopherus31.5@gmail.com>
2023-11-20 11:29:58 +0100

    [dbs-leipzig#1559] First commit, got the code from the BigDataPraktikum Group

---
+++ gradoop-temporal/src/main/java/org/gradoop/temporal/model/impl/operators/metric/MinDegreeEvolution.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2014 - 2021 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradoop.temporal.model.impl.operators.metric;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.gradoop.flink.model.impl.operators.sampling.functions.VertexDegree;
+import org.gradoop.temporal.model.api.TimeDimension;
+import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.gradoop.temporal.model.impl.operators.metric.functions.AggregationType;
+import org.gradoop.temporal.model.impl.operators.metric.functions.GroupDegreeTreesToAggregateDegrees;
+
+/**
+ * Operator that calculates the evolution of the graph's minimum degree for the whole lifetime of the graph.
+ * The result is a triple dataset {@link DataSet<Tuple3>} in form {@code <Long, Long, Float>}. It
+ * represents a time interval (first and second element) and the aggregated degree value for this interval
+ * (3rd element).
+ */
+public class MinDegreeEvolution extends BaseAggregateDegreeEvolution {
+
+    /**
+     * Creates an instance of this minimum degree evolution operator using {@link TimeDimension#VALID_TIME}
+     * as default time dimension and {@link VertexDegree#BOTH} as default degree type.
+     */
+    public MinDegreeEvolution() {
+        super();
+    }
+
+    /**
+     * Creates an instance of this minimum degree evolution operator using the given time dimension and
+     * degree type.
+     *
+     * @param degreeType the degree type (IN, OUT or BOTH)
+     * @param dimension the time dimension to consider (VALID_TIME or TRANSACTION_TIME)
+     */
+    public MinDegreeEvolution(VertexDegree degreeType, TimeDimension dimension) {
+        super(degreeType, dimension);
+    }
+
+    @Override
+    public DataSet<Tuple3<Long, Long, Float>> execute(TemporalGraph graph) {
+        return preProcess(graph).reduceGroup(new GroupDegreeTreesToAggregateDegrees(AggregationType.MIN));
+    }
+}
---
+++ gradoop-temporal/src/main/java/org/gradoop/temporal/model/impl/operators/metric/functions/GroupDegreeTreesToAggregateDegrees.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright © 2014 - 2021 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradoop.temporal.model.impl.operators.metric.functions;
+
+import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.util.Collector;
+import org.gradoop.common.model.impl.id.GradoopId;
+
+import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.SortedSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A group reduce function that merges all Tuples (vId, degreeTree) to a dataset of tuples (time, aggDegree)
+ * that represents the aggregated degree value for the whole graph at the given time.
+ */
+public class GroupDegreeTreesToAggregateDegrees
+        implements GroupReduceFunction<Tuple2<GradoopId, TreeMap<Long, Integer>>, Tuple3<Long, Long, Float>> {
+
+    /**
+     * The aggregate type to use (min,max,avg).
+     */
+    private final AggregationType aggregateType;
+
+    /**
+     * Creates an instance of this group reduce function.
+     *
+     * @param aggregateType the aggregate type to use (min,max,avg).
+     */
+    public GroupDegreeTreesToAggregateDegrees(AggregationType aggregateType) {
+        this.aggregateType = aggregateType;
+    }
+
+    @Override
+    public void reduce(Iterable<Tuple2<GradoopId, TreeMap<Long, Integer>>> iterable,
+                       Collector<Tuple3<Long, Long, Float>> collector) throws Exception {
+
+        // init necessary maps and set
+        HashMap<GradoopId, TreeMap<Long, Integer>> degreeTrees = new HashMap<>();
+        HashMap<GradoopId, Integer> vertexDegrees = new HashMap<>();
+        SortedSet<Long> timePoints = new TreeSet<>();
+
+        // convert the iterables to a hashmap and remember all possible timestamps
+        for (Tuple2<GradoopId, TreeMap<Long, Integer>> tuple : iterable) {
+            degreeTrees.put(tuple.f0, tuple.f1);
+            timePoints.addAll(tuple.f1.keySet());
+        }
+
+        int numberOfVertices = degreeTrees.size();
+        long lastTimestamp = Long.MIN_VALUE;
+        float degreeValue = 0f;
+
+        // Add default times
+        timePoints.add(Long.MIN_VALUE);
+
+        for (Long timePoint : timePoints) {
+            // Do the collection from the previous loop
+            if (lastTimestamp < timePoint) {
+                collector.collect(new Tuple3<>(lastTimestamp, timePoint, degreeValue));
+            }
+
+            // skip last default time
+            if (Long.MAX_VALUE == timePoint) {
+                continue;
+            }
+
+            // Iterate over all vertices
+            for (Map.Entry<GradoopId, TreeMap<Long, Integer>> entry : degreeTrees.entrySet()) {
+                // Make sure the vertex is registered in the current vertexDegrees capture
+                if (!vertexDegrees.containsKey(entry.getKey())) {
+                    vertexDegrees.put(entry.getKey(), 0);
+                }
+
+                // Check if timestamp is in tree, if not, take the lower key
+                if (entry.getValue().containsKey(timePoint)) {
+                    vertexDegrees.put(entry.getKey(), entry.getValue().get(timePoint));
+                } else {
+                    Long lowerKey = entry.getValue().lowerKey(timePoint);
+                    if (lowerKey != null) {
+                        vertexDegrees.put(entry.getKey(), entry.getValue().get(lowerKey));
+                    }
+                }
+            }
+
+            lastTimestamp = timePoint;
+            // Here, every tree with this time point is iterated. Now we need to aggregate for the current time.
+            switch (aggregateType) {
+                case MIN:
+                    degreeValue = vertexDegrees.values().stream().reduce(Math::min).orElse(0).floatValue();
+                    break;
+                case MAX:
+                    degreeValue = vertexDegrees.values().stream().reduce(Math::max).orElse(0).floatValue();
+                    break;
+                case AVG:
+                    int sum = vertexDegrees.values().stream().reduce(Math::addExact).orElse(0);
+                    degreeValue = (float) sum /  (float) numberOfVertices;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Aggregate type not specified.");
+            }
+            // Collect in next iteration
+        }
+    }
+}
